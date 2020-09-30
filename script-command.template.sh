bdb86b950c6a1448320da580dda190ae87774b48
Petr Nikolaev <pitnikola@gmail.com>
2020-09-30 15:50:45 +0100

    Add template for script command

---
+++ script-command.template.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Raycast Script Command Template
+#
+# Duplicate this file and remove ".template." from the filename to get started.
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title My First Script
+# @raycast.mode fullOutput
+#
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.currentDirectoryPath ~
+# @raycast.packageName Raycast Scripts
+
+echo "Hello from My First Script"
