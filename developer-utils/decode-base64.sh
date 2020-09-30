dc7e755266d176ce48cabf96ffd06abfcb93904c
Thomas Paul Mann <hi@thomaspaulmann.com>
2020-09-30 12:56:01 +0100

    Add command to encode/decode base64

---
+++ developer-utils/decode-base64.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Decode Base64
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 💻
+
+pbpaste | base64 -d | pbcopy
---
+++ developer-utils/encode-base64.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Encode Base64
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 💻
+
+pbpaste | base64 | pbcopy
