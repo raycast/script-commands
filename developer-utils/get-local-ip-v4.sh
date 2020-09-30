703a729bc43f9ca5c4f43c8a06d1e4f656a2a735
Thomas Paul Mann <hi@thomaspaulmann.com>
2020-09-30 13:18:04 +0100

    Add commands to get IPs

---
+++ developer-utils/get-local-ip-v4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Local IPv4
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🌐
+# @raycast.packageName Internet
+
+ip=$(ifconfig | grep 'inet.*broadcast' | awk '{print $2}')
+echo ip | pbcopy
+
+echo "Copied $ip"
---
+++ developer-utils/get-local-ip-v6.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Local IPv6
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🌐
+# @raycast.packageName Internet
+
+ip=$(ifconfig | grep 'inet6.*%en' | awk '{print $2}')
+echo ip | pbcopy
+
+echo "Copied $ip"
