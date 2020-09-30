f0f33e38368444fc222d8efa1c6e47fefd34271e
Thomas Paul Mann <hi@thomaspaulmann.com>
2020-09-30 13:18:04 +0100

    Add commands to get IPs

---
+++ developer-utils/get-external-ip-v6.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title External IPv6
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🌐
+# @raycast.packageName Internet
+
+ip=$(curl -6 -s -m 5 https://ifconfig.co)
+echo ip | pbcopy
+
+echo "Copied $ip"
