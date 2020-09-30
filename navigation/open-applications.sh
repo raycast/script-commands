c1aa9b03edaca35ed3750d667f8f9926bc39de1d
Thomas Paul Mann <hi@thomaspaulmann.com>
2020-09-30 12:56:32 +0100

    Add commands to open common directories

---
+++ navigation/open-applications.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Applications
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📂
+
+open ~/Applications
---
+++ navigation/open-library.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Library
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📂
+
+open ~/Library
