fc9377c92b86665d6768b42f2290ed5f5d50e215
Thomas Paul Mann <hi@thomaspaulmann.com>
2020-09-30 12:56:32 +0100

    Add commands to open common directories

---
+++ navigation/open-desktop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Desktop
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📂
+
+open ~/Desktop
