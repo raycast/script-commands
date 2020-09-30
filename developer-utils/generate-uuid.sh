2c3303915bcdb8f0e7eb5e73ff2dc029889873fd
Thomas Paul Mann <hi@thomaspaulmann.com>
2020-09-30 12:56:11 +0100

    Add command to generate uuid

---
+++ developer-utils/generate-uuid.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Generate UUID
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 💻
+
+uuidgen | pbcopy
