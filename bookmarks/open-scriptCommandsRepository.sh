154fc149c1016b3573d85348c304142d9b231814
Petr Nikolaev <pitnikola@gmail.com>
2020-10-02 16:04:30 +0100

    Add command to quickly open this repository on GitHub

---
+++ bookmarks/open-scriptCommandsRepository.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Script Commands Repository
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📜
+
+open https://github.com/raycast/script-commands
