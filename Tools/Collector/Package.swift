e77bc9055a73481652720171ac3b449da193fb94
Thiago Holanda <unnamedd@gmail.com>
2020-10-03 21:43:37 +0200

    Add ArgumentParser as dependency

--- Tools/Collector/Package.swift
+++ Tools/Collector/Package.swift
@@ -9,13 +9,18 @@ let package = Package(
             url: "https://github.com/apple/swift-tools-support-core.git", 
             .branch("master")
         ),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser.git", 
+            .upToNextMinor(from: "0.3.0")
+        ),
     ],
     targets: [
         .target(
             name: "Collector",
             dependencies: [
                 "CollectorKit",
-                .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core")        
+                .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
             ]
         ),
         .target(
