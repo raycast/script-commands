1807536809a139194ad9e25944a7f6b96bb51400
Thiago Holanda <unnamedd@gmail.com>
2020-10-04 05:44:53 +0200

    Add placeholder for ValidateScript sub command

---
+++ Tools/Collector/Sources/Collector/SubCommands/ValidateScripts.swift
@@ -0,0 +1,15 @@
+// 
+//  MIT License
+//  Copyright (c) 2020 Raycast Technologies Ltd. All rights reserved.
+// 
+
+import ArgumentParser
+import CollectorKit
+
+extension CollectorCommand {
+    struct ValidateScripts: ParsableCommand {
+        func run() throws {
+            print("To be implemented")
+        }
+    }
+}
