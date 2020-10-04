024980032b253bb2b1b3a73795d9fc9fcf13124e
Thiago Holanda <unnamedd@gmail.com>
2020-10-05 00:12:10 +0200

    Add conformance to MarkdownDescriptionProtocol

--- Tools/Collector/Sources/CollectorKit/Models/ScriptCommand.swift
+++ Tools/Collector/Sources/CollectorKit/Models/ScriptCommand.swift
@@ -1,7 +1,7 @@
-// 
+//
 //  MIT License
 //  Copyright (c) 2020 Raycast Technologies Ltd. All rights reserved.
-// 
+//
 
 import Foundation
 
@@ -14,8 +14,10 @@ struct ScriptCommand: Codable {
     var packageName: String?
     let icon: String?
     let author: Author?
-    let description: String?
+    let details: String?
     let currentDirectoryPath: String?
+    
+    private var groupPath: String?
     
     enum CodingKeys: String, CodingKey {
         case schemaVersion
@@ -24,9 +26,37 @@ struct ScriptCommand: Codable {
         case packageName
         case icon
         case author
-        case description
+        case details = "description"
         case currentDirectoryPath
     }
+    
+    var iconString: String {
+        guard let icon = icon, icon.isEmpty == false else {
+            return .empty
+        }
+        
+        if icon.isEmoji {
+            return icon
+        }
+        
+        guard let groupPath = self.groupPath else {
+            return .empty
+        }
+        
+        let imageURL = "https://raw.githubusercontent.com/raycast/script-commands/master/\(groupPath)/\(icon)?raw=true"
+        let image = "![](\(imageURL))"
+        
+        return image
+    }
+    
+    mutating func setGroupPath(for group: Group) {
+        self.groupPath = group.path
+    }
+}
+
+// MARK: - Encode/Decode
+
+extension ScriptCommand {
     
     init(from decoder: Decoder) throws {
         let container               = try decoder.container(keyedBy: CodingKeys.self)
@@ -40,9 +70,9 @@ struct ScriptCommand: Codable {
         self.mode                   = try container.decodeIfPresent(Mode.self, forKey: .mode)
         self.packageName            = try container.decodeIfPresent(String.self, forKey: .packageName)
         self.icon                   = try container.decodeIfPresent(String.self, forKey: .icon)
-        self.description            = try container.decodeIfPresent(String.self, forKey: .description)
+        self.details                = try container.decodeIfPresent(String.self, forKey: .details)
         self.currentDirectoryPath   = try container.decodeIfPresent(String.self, forKey: .currentDirectoryPath)
-
+        
         let name = try authorContainer.decodeIfPresent(String.self, forKey: .name)
         let url = try authorContainer.decodeIfPresent(String.self, forKey: .url)
         
@@ -61,16 +91,19 @@ struct ScriptCommand: Codable {
     
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-    
+        
         try container.encode(schemaVersion, forKey: .schemaVersion)
         try container.encode(title, forKey: .title)
         try container.encode(mode, forKey: .mode)
         try container.encode(packageName, forKey: .packageName)
         try container.encode(icon, forKey: .icon)
-        try container.encode(description, forKey: .description)
+        try container.encode(details, forKey: .details)
         try container.encode(currentDirectoryPath, forKey: .currentDirectoryPath)
         try container.encode(author, forKey: .author)
     }
+    
+}
+
 // MARK: - Comparable
 
 extension ScriptCommand: Comparable {
@@ -92,40 +125,42 @@ extension ScriptCommand: Comparable {
 extension ScriptCommand: MarkdownDescriptionProtocol {
     
     var markdownDescription: String {
-        var content = """
-        \n\n
+        var content: String = .empty
+        
+        let header = """
         | \(iconString) | \(title) |
         | ------------| ---------- |
         """
         
+        content += .newLine + header
+        
         if let value = details {
-            content += "\n| Description | \(value) |"
+            content += .newLine + "| Description | \(value) |"
         }
         
         if let value = author {
-            content += "\n| Author | \(value.markdownDescription) |"
-        }
-
-        if let value = packageName {
-            content += "\n| Package Name | \(value.capitalized) |"
-        }
-
-        if let value = mode {
-            content += "\n| Package Name | \(value.description) |"
+            content += .newLine + "| Author | \(value.markdownDescription) |"
         }
         
-        content += "\n| Schema Version | \(schemaVersion) |"
+        if let value = packageName {
+            content += .newLine + "| Package Name | \(value.capitalized) |"
+        }
+        
+        if let value = mode {
+            content += .newLine + "| Mode | \(value.rawValue) |"
+        }
+        
+        content += .newLine + "| Schema Version | \(schemaVersion) |"
         
         if let value = currentDirectoryPath {
-            content += "\n| Current Path Directory | \(value) |"
+            content += .newLine + "| Current Path Directory | \(value) |"
         }
         
         return content
     }
     
     var sectionTitle: String {
-        ""
+        .empty
     }
     
-}
 }
