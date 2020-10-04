298d57fd9a9a08de7d6f0e92db62ee0530b52df1
Thiago Holanda <unnamedd@gmail.com>
2020-10-05 00:11:22 +0200

    Conform models to protocol Comparable

--- Tools/Collector/Sources/CollectorKit/Models/Groups.swift
+++ Tools/Collector/Sources/CollectorKit/Models/Groups.swift
@@ -11,3 +11,15 @@ struct Group: Codable {
     let name: String
     let scriptCommands: ScriptCommands
 }
+// MARK: - Comparable
+
+extension Group: Comparable {
+    static func < (lhs: Group, rhs: Group) -> Bool {
+        lhs.name < rhs.name
+    }
+    
+    static func == (lhs: Group, rhs: Group) -> Bool {
+        lhs.name == rhs.name
+    }
+    
+}
