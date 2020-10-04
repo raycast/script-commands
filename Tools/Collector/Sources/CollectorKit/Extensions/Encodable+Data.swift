9f1a2d068d491fb0b19de81e5683d8f9996e7d54
Thiago Holanda <unnamedd@gmail.com>
2020-10-04 05:46:51 +0200

    Minor improvement for the JSON generated by the JSONEncoder

--- Tools/Collector/Sources/CollectorKit/Extensions/Encodable+Data.swift
+++ Tools/Collector/Sources/CollectorKit/Extensions/Encodable+Data.swift
@@ -8,6 +8,12 @@ import Foundation
 extension Encodable {
     func toData() throws -> Data {
         let encoder = JSONEncoder()
+        encoder.outputFormatting.insert(.prettyPrinted)
+        
+        if #available(macOS 10.13, *) {
+            encoder.outputFormatting.insert(.sortedKeys)
+        }
+        
         return try encoder.encode(self)
     }
 }
