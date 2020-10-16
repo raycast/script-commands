ebfc735e726aa8c8cf310ab3140e4606580afd29
fe9lix <fe9lix@googlemail.com>
2020-10-16 17:38:24 +0200

    Add demo dashboard script for system activity (CPU, MEM)

---
+++ dashboard/system-activity.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title System Activity
+# @raycast.mode inline
+# @raycast.refreshTime 5s
+
+# Optional parameters:
+# @raycast.icon 📈
+
+cpu_mem=$(ps -A -o %cpu,%mem | awk '{ cpu += $1; mem += $2} END {print "CPU: "cpu"% MEM: "mem"%"}')
+
+echo "${cpu_mem}"
