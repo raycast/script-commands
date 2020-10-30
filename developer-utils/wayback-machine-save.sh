48de2085f753fbfd45b37af5ecc959a790be4f0f
Caleb Stauffer <develop+github@calebstauffer.com>
2020-10-30 16:44:15 -0400

    add script command: Wayback Machine Save

---
+++ developer-utils/wayback-machine-save.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Wayback Machine - Save clipboard URL
+# @raycast.mode compact
+# @raycast.author Caleb Stauffer
+# @raycast.authorURL https://github.com/crstauf
+# @raycast.description Save URL in clipboard to Wayback Machine
+
+# Optional parameters:
+# @raycast.icon images/ia-logo.jpg
+
+url=$(pbpaste)
+regex='(https?)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+
+if [[ $url =~ $regex ]]
+then 
+	curl -s "http://web.archive.org/save/$url" > /dev/null
+	echo "Submitted $url to Wayback Machine"
+else
+	echo "$url (in clipboard) is not valid URL"
+fi
