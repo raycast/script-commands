6428c42a79c11ec16601e4e3a4cb5644027778d4
Caleb Stauffer <develop+github@calebstauffer.com>
2020-10-31 13:18:27 -0400

    add script command: Whois

---
+++ developer-utils/whois.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Whois of clipboard URL
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.author Caleb Stauffer
+# @raycast.authorURL https://github.com/crstauf
+# @raycast.description Whois of clipboard URL.
+# @raycast.packageName Internet
+# @raycast.needsConfirmation true
+# @raycast.icon 🌐
+
+clipboard=$(pbpaste)
+
+open https://who.is/whois/$clipboard
