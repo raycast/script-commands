23c23398d882eda1dd07c8394e62bf53391136d7
Caleb Stauffer <develop+github@calebstauffer.com>
2020-10-30 22:03:08 -0400

    add package name

--- developer-utils/wayback-machine-save.sh
+++ developer-utils/wayback-machine-save.sh
@@ -10,6 +10,7 @@
 # @raycast.authorURL https://github.com/crstauf
 # @raycast.description Save clipboard URL to Wayback Machine
 # @raycast.icon images/ia-logo.jpg
+# @raycast.packageName Internet Archive
 
 url=$(pbpaste)
 regex='(https?)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
