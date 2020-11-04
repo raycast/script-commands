19f33f6b66c09d70b4c45e709e815fa688d6ae98
maximkrouk <id.maximkrouk@gmail.com>
2020-11-04 16:22:36 +0300

    Record simulator script improvements

--- developer-utils/record-simulator.sh
+++ developer-utils/record-simulator.sh
@@ -6,6 +6,10 @@
 # @raycast.mode compact
 
 # Optional parameters:
+# @raycast.author Maxim Krouk
+# @raycast.authorURL https://github.com/maximkrouk
+# @raycast.description Records simulator to Downloads folder with a filename from the clipboard
+# @raycast.needsConfirmation true
 # @raycast.icon 📱
 
 clipboard=$(pbpaste)
