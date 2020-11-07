5e9da40008c2f0f058bb56a019e5108e863f3295
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-07 14:00:32 -0500

    add script commands: TV

---
+++ system/tv-play.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/osascript
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Play
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.author Caleb Stauffer
+# @raycast.authorURL https://github.com/crstauf
+# @raycast.description Play TV
+# @raycast.packageName TV
+# @raycast.icon images/tv-logo.png
+
+tell application "TV"
+	if player state is paused then 
+		play
+		do shell script "echo Playing"
+	end if
+end tell
