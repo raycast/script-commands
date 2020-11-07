fa954be1daba68771fbeba2dffac5c108f7f22e2
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-07 14:59:48 -0500

    add script commands: Cloudup

---
+++ communication/cloudup-pick.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/osascript
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Pick
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.author Caleb Stauffer
+# @raycast.authorURL https://github.com/crstauf
+# @raycast.description Open file dialog and upload to Cloudup
+# @raycast.packageName Cloudup
+# @raycast.icon images/cloudup-logo.png
+
+tell application "Cloudup" to pick
