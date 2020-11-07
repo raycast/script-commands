d9a0274a1a3ee5b0d2dde067f52f587ce57e6998
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-07 11:17:15 -0500

    shorten location to city

--- dashboard/current-weather.sh
+++ dashboard/current-weather.sh
@@ -12,5 +12,9 @@
 # @raycast.description Get current weather report from wttr.in.
 # @raycast.icon 🌦️
 
-output=$(curl -s https://wttr.in/?format="%c+%f+%w+in+%l")
-echo $output
+output=$(curl -s https://wttr.in/?format="%c+%f+%w{;}%l")
+
+IFS='{;}' read -ra array <<< "$output"
+IFS=',' read -ra location <<< "${array[1]}"
+
+echo "${array[0]} - ${location[0]}"
