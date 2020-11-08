ed92c6857118bb49abefc998b104afd5f893d3f2
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-08 15:14:25 -0500

    add report URL to output

--- dashboard/speedtest.sh
+++ dashboard/speedtest.sh
@@ -22,6 +22,8 @@ fi
 
 json=$(speedtest -f json-pretty)
 
+report_url=$(echo "$json" | jq -r '.result.url')
+
     ping=$(echo "$json" | jq -r '.ping.latency')
 bps_down=$(echo "$json" | jq -r '.download.bandwidth')
   bps_up=$(echo "$json" | jq -r '.upload.bandwidth')
@@ -31,3 +33,4 @@ mbps_down=$(echo "scale=2; $bps_down / $divide_to_mbps" | bc)
   mbps_up=$(echo "scale=2;   $bps_up / $divide_to_mbps" | bc)
 
 echo "↓ ${mbps_down}mbps  ↑ ${mbps_up}mbps  ↔ ${ping}ms"
+echo "Full report: $report_url"
