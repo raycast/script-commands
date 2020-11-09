fade2faed81849d3f96bc8f82d1936a645e79821
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-09 15:37:42 -0500

    add script commands: ping

---
+++ developer-utils/ping-monitor.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# IP address or URL
+# target=""
+
+# @raycast.schemaVersion 1
+# @raycast.title Ping Monitor
+# @raycast.mode inline
+# @raycast.author Caleb Stauffer
+# @raycast.authorURL https://github.com/crstauf
+# @raycast.description Ping an IP address or URL on an interval.
+# @raycast.packageName Internet
+# @raycast.refreshTime 30m
+# @raycast.icon 🌐
+
+if [ -z ${target+x} ]; then
+	echo "Target is undefined.";
+	exit 0
+fi
+
+output=$(ping -i 0.25 -t 3 -q "$target")
+summary=$(echo "$output" | awk 'END{print}')
+
+IFS=' ' read -ra array <<< $summary
+times=$(echo ${array[3]})
+
+avg=$(echo $times | awk -F/ '{print $2}')
+
+echo "${target}: ${avg}${array[4]}"
