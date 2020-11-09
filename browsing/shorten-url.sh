5b721d5bb56433554b380e2e5b027ef92139cd88
Thomas Paul Mann <hi@thomaspaulmann.com>
2020-11-09 17:16:09 +0000

    Add command to shorten URL

---
+++ browsing/shorten-url.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Shorten URL From Clipboard
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🔗
+
+# Documentation:
+# @raycast.author Thomas Paul Mann
+# @raycast.authorURL https://github.com/thomaspaulmann
+# @raycast.description Shorten the URL in your Clipboard with Tiny URL.
+
+regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+pasteboardString=$(pbpaste)
+
+if [[ $pasteboardString =~ $regex ]] 
+then 
+	result=$(curl "https://tinyurl.com/api-create.php?url=$pasteboardString")
+	if [[ $result != "Error" ]]
+	then
+		echo $result | pbcopy
+		echo "Copied shorten URL"
+	else 
+		echo "URL cannot be shorten"
+		exit 1
+	fi
+else
+	echo "String in clipboard is a not valid URL"
+	exit 1
+fi
