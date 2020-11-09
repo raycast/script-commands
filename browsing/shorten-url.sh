78f6aa6bf2a2b5f1297260bda433efbc731b4859
Thomas Paul Mann <hi@thomaspaulmann.com>
2020-11-09 19:08:51 +0000

    Make shorten URL a silent command

--- browsing/shorten-url.sh
+++ browsing/shorten-url.sh
@@ -3,7 +3,7 @@
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title Shorten URL From Clipboard
-# @raycast.mode compact
+# @raycast.mode silent
 
 # Optional parameters:
 # @raycast.icon 🔗
