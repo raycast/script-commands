c1d09a1b3a4ae4a44d29ad207372dbd403b880f6
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-12 17:21:40 -0500

    add script command: xkcdpass

---
+++ communication/xkcdpass.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title xkcdpass
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.author Caleb Stauffer
+# @raycast.authorURL https://github.com/crstauf
+# @raycast.description Use xkcdpass to create a passphrase.
+# @raycast.packageName Communication
+# @raycast.icon 🔐
+
+if ! command -v xkcdpass &> /dev/null; then
+	echo "xkcdpass is required (https://pypi.org/project/xkcdpass/).";
+	exit 1;
+fi
+
+xkcdpass | pbcopy
+
+echo "Copied passphrase"
