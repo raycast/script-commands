3275c03b85b0761a16bc881b997158d522ccd09c
Sven Hofmann <info@hofmannsven.com>
2020-11-12 20:31:09 +0100

    Add script command: generate password

---
+++ commands/developer-utils/generate-password.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Generate Password
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.author Sven Hofmann
+# @raycast.authorURL https://github.com/hofmannsven
+# @raycast.description Generates a random password and copies it to the clipboard.
+# @raycast.icon 💻
+
+gpg --gen-random -a 0 30 | pbcopy
+echo "Password Generated"
