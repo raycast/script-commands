164aebafc4288478077c52ed9c118a45a4b06ba3
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-13 02:17:44 -0500

    add script command: download-url

---
+++ browsing/download-url.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Download URL in clipboard
+# @raycast.mode silent
+# @raycast.author Caleb Stauffer
+# @raycast.authorURL https://github.com/crstauf
+# @raycast.description Download URL in clipboard.
+# @raycast.packageName Developer Utilities
+# @raycast.needsConfirmation true
+# @raycast.currentDirectoryPath ~/Desktop
+
+cmnd=$(npm config get prefix)/bin/download
+
+if ! command -v $cmnd &> /dev/null; then
+	echo "download command is required (https://github.com/kevva/download-cli).";
+	exit 1;
+fi
+
+clipboard=$(pbpaste)
+regex='(https?)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+
+if [[ $clipboard =~ $regex ]]; then 
+	"$cmnd" -o ~/Desktop $clipboard
+	echo "Downloaded URL to Desktop"
+else
+	echo "Clipboard is not a valid URL"
+	exit 1
+fi
