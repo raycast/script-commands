b3347def0a9dee1091b800d44cd265301b3139b9
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-13 12:36:17 -0500

    move Cloudup script commands into sub-directory

--- communication/cloudup/cloudup-pick.sh
+++ communication/cloudup/cloudup-pick.sh
@@ -2,18 +2,14 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Upload
+# @raycast.title Pick
 # @raycast.mode silent
 
 # Optional parameters:
 # @raycast.author Caleb Stauffer
 # @raycast.authorURL https://github.com/crstauf
-# @raycast.description Upload path or URL in clipboard to Cloudup
+# @raycast.description Open file dialog and upload to Cloudup
 # @raycast.packageName Cloudup
-# @raycast.icon images/cloudup-logo.png
+# @raycast.icon cloudup-logo.png
 
-set variable to (do shell script "pbpaste")
-
-tell application "Cloudup"
-	upload variable
-end tell
+tell application "Cloudup" to pick
--- communication/cloudup/cloudup-upload.sh
+++ communication/cloudup/cloudup-upload.sh
@@ -2,14 +2,18 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Paste
+# @raycast.title Upload
 # @raycast.mode silent
 
 # Optional parameters:
 # @raycast.author Caleb Stauffer
 # @raycast.authorURL https://github.com/crstauf
-# @raycast.description Upload clipboard contents to Cloudup
+# @raycast.description Upload path or URL in clipboard to Cloudup
 # @raycast.packageName Cloudup
-# @raycast.icon images/cloudup-logo.png
+# @raycast.icon cloudup-logo.png
 
-tell application "Cloudup" to paste
+set variable to (do shell script "pbpaste")
+
+tell application "Cloudup"
+	upload variable
+end tell
