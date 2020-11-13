00bb863974825934615715ee4da0d69cdf6af516
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-13 12:19:48 -0500

    remove weirdness! (thanks to @unnamedd)

--- browsing/website-screenshots.sh
+++ browsing/website-screenshots.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 
 # Set viewport resolution of screenshots
 resolutions="1920x1080 390x844"
@@ -13,23 +13,21 @@ resolutions="1920x1080 390x844"
 # @raycast.needsConfirmation true
 # @raycast.currentDirectoryPath ~/Desktop
 
-cmnd=$(npm config get prefix)/bin/pageres
-
-if ! command -v $cmnd &> /dev/null; then
+if ! command -v pageres &> /dev/null; then
 	echo "pageres command is required (https://github.com/sindresorhus/pageres-cli).";
 	exit 1;
 fi
 
 if [ -z ${resolutions+x} ]; then
-echo "Viewport resolutions are undefined.";
-exit 0
+	echo "Viewport resolutions are undefined.";
+	exit 0
 fi
 
 regex='(https?)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
 clipboard=$(pbpaste)
 
 if [[ $clipboard =~ $regex ]]; then 
-	output=$($cmnd --crop "$clipboard" $resolutions)
+	output=$(pageres --crop "$clipboard" $resolutions)
 	echo "Saved screenshot(s) to Desktop"
 else
 	echo "Clipboard does not contain a valid url"
