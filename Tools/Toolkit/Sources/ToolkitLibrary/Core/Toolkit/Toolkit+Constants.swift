9b77ae3e560512caddcc3d95c605587cf4ce8187
Thiago Holanda <unnamedd@gmail.com>
2020-11-14 05:45:16 +0100

    Moved blockedFolderList out of the Toolkit initialiser

--- Tools/Toolkit/Sources/ToolkitLibrary/Core/Toolkit/Toolkit+Constants.swift
+++ Tools/Toolkit/Sources/ToolkitLibrary/Core/Toolkit/Toolkit+Constants.swift
@@ -7,6 +7,31 @@ import Foundation
 import TSCBasic
 
 public extension Toolkit {
+  
+  internal var blockedFolderList: [String] {
+    [
+      ".git",
+      "screenshots",
+      "Tools",
+      ".build",
+      ".github",
+      "templates",
+      "images",
+      "_enabled-commands"
+    ]
+  }
+  
+  static var information: (name: String, version: String) {
+    (
+      name: "Raycast Toolkit",
+      version: "0.1"
+    )
+  }
+  
+  static func raycastDescription() {
+    Console.shared.write(string: information.name, color: .red, bold: true)
+  }
+
   static func version() {
     raycastDescription()
     Console.shared.write("Current version: \(information.version)")
