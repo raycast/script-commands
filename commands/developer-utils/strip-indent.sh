0c169972d12a02bbe8211ad667e0d88348226a5e
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-16 15:43:12 -0500

    add script command: strip-indent

---
+++ commands/developer-utils/strip-indent.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Dependency: requires strip-indent (https://github.com/sindresorhus/strip-indent-cli)
+# Install via npm: `npm install --global strip-indent-cli`
+
+# @raycast.schemaVersion 1
+# @raycast.title Shift clipboard contents left
+# @raycast.mode silent
+# @raycast.author Caleb Stauffer
+# @raycast.authorURL https://github.com/crstauf
+# @raycast.description Strip leading whitespace from each line in a string.
+# @raycast.packageName Developer Utilities
+# @raycast.needsConfirmation true
+# @raycast.icon 📋
+
+if ! command -v strip-indent &> /dev/null; then
+	echo "strip-indent command is required (https://github.com/sindresorhus/strip-indent-cli).";
+	exit 1;
+fi
+
+clipboard=$(pbpaste)
+
+# The line with the least number of leading whitespace, ignoring empty lines, determines the number to remove.
+echo "$clipboard" | strip-indent | pbcopy
+
+echo "Copied left-shifted content"
