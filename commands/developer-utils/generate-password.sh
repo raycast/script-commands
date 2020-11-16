16802d55abffd5b07e92266d83d6e3ff7992f299
Sven Hofmann <info@hofmannsven.com>
2020-11-16 21:06:10 +0100

    Add check for dependencies.

--- commands/developer-utils/generate-password.sh
+++ commands/developer-utils/generate-password.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Dependecy: requires gpg (https://www.gnupg.org/)
+# Download: https://www.gnupg.org/download/#sec-1-1
+
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title Generate Password
@@ -10,6 +13,11 @@
 # @raycast.authorURL https://github.com/hofmannsven
 # @raycast.description Generates a random password and copies it to the clipboard.
 # @raycast.icon 🔐
+
+if ! command -v gpg &> /dev/null; then
+	echo "gpg command is required (https://www.gnupg.org/).";
+	exit 1;
+fi
 
 gpg --gen-random -a 0 30 | pbcopy
 echo "Password Generated"
