2b280ca66d56827abb74fe7e3e4069af5cfb6dcd
Thiago Holanda <unnamedd@gmail.com>
2020-11-17 22:27:11 +0100

    Update Raycast Community URL

--- Tools/Toolkit/Sources/ToolkitLibrary/Core/Documentation/Documentation.swift
+++ Tools/Toolkit/Sources/ToolkitLibrary/Core/Documentation/Documentation.swift
@@ -94,7 +94,7 @@ private extension Documentation {
         This is a shared place and we're always looking for new Script Commands or other ways to improve Raycast.
         If you have anything cool to show, please send us a pull request. If we screwed something up,
         please report a bug. Join our
-        [Slack community](https://join.slack.com/t/raycastcommunity/shared_invite/zt-hhzj9i4m-D5~HwnTRsJKrcZmVDJ4mkg)
+        [Slack community](https://www.raycast.com/community)
         to brainstorm ideas with like-minded folks.
         """
 
