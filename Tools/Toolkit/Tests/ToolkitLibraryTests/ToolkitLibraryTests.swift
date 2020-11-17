192993d7caf816b98075ccc817b67895bfef21c1
Thiago Holanda <unnamedd@gmail.com>
2020-11-17 22:34:33 +0100

    Remove unnecessary code in tests

--- Tools/Toolkit/Tests/ToolkitLibraryTests/ToolkitLibraryTests.swift
+++ Tools/Toolkit/Tests/ToolkitLibraryTests/ToolkitLibraryTests.swift
@@ -3,15 +3,6 @@ import class Foundation.Bundle
 
 final class ToolkitLibraryTests: XCTestCase {
   func testExample() throws {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct
-    // results.
-    
-    // Some of the APIs that we use below are available in macOS 10.13 and above.
-    guard #available(macOS 10.13, *) else {
-      return
-    }
-    
     let fooBinary = productsDirectory.appendingPathComponent("toolkit")
     
     let process = Process()
