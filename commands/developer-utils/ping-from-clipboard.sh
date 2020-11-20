f10173da5aadf70a98df6dd3e172ae4b2fb23eee
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-20 08:30:19 -0500

    enhance ping

--- commands/developer-utils/ping-from-clipboard.sh
+++ commands/developer-utils/ping-from-clipboard.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # @raycast.schemaVersion 1
-# @raycast.title Ping Address in Clipboard
+# @raycast.title Ping
 # @raycast.mode fullOutput
 # @raycast.author Caleb Stauffer
 # @raycast.authorURL https://github.com/crstauf
-# @raycast.description Ping an IP address or URL in clipboard.
+# @raycast.description Ping an IP address or URL.
 # @raycast.packageName Internet
-# @raycast.needsConfirmation true
 # @raycast.icon 🌐
+# @raycast.argument1 { "type": "text", "placeholder": "URL or IP address" }
 
-clipboard=$(pbpaste)
-ping -i 0.25 -t 3 "$clipboard"
+ping -i 0.25 -t 3 "$1"
