a91efc9d013ce6767e076a6b4fb8869b6a7a2bfe
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-20 08:16:57 -0500

    enhance emoji-copy and emojis-search

--- commands/communication/emojis/emojis-search.sh
+++ commands/communication/emojis/emojis-search.sh
@@ -4,30 +4,28 @@
 # Install via npm: `npm install --global emoj`
 
 # @raycast.schemaVersion 1
-# @raycast.title Search Related Emojis from Clipboard Content
+# @raycast.title Search Emojis
 # @raycast.mode fullOutput
 # @raycast.author Caleb Stauffer
 # @raycast.authorURL https://github.com/crstauf
-# @raycast.description Search related emojis to clipboard content.
-# @raycast.needsConfirmation true
+# @raycast.description Search related emojis to input.
 # @raycast.packageName Communication
 # @raycast.icon 📙
+# @raycast.argument1 { "type": "text", "placeholder": "Search for..." }
 
 if ! command -v emoj &> /dev/null; then
 	echo "emoj command is required (https://github.com/sindresorhus/emoj).";
 	exit 1;
 fi
 
-clipboard=$(pbpaste)
-
-emojis=$(emoj "$clipboard")
+emojis=$(emoj "$1")
 
 if [ -z "$emojis" ]; then
-	echo "No emojis found for \"${clipboard}\""
+	echo "No emojis found for \"${1}\""
 	exit 0
 fi
 
-echo "Emojis found for \"$clipboard\":"
+echo "Emojis found for \"$1\":"
 echo ""
 echo "$emojis"
 echo ""
