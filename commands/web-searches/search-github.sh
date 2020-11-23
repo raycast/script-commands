ad05e623b44b36c8b362ac392497425646f942cf
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-23 14:21:53 -0500

    fix to not be cdnjs search

--- commands/web-searches/search-github.sh
+++ commands/web-searches/search-github.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # @raycast.schemaVersion 1
-# @raycast.title Search cdnjs
+# @raycast.title Search GitHub
 # @raycast.mode silent
 # @raycast.author Caleb Stauffer
 # @raycast.authorURL https://github.com/crstauf
-# @raycast.description Search cdnjs.com for library.
+# @raycast.description Search GitHub.
 # @raycast.icon images/cloudflare-logo.png
-# @raycast.argument1 { "type": "text", "placeholder": "Library" }
+# @raycast.argument1 { "type": "text", "placeholder": "Query" }
 # @raycast.packageName Web Searches
 
-open "https://cdnjs.com/libraries?q=${1// /%20}"
+open "https://github.com/search?q=${1// /%20}"
