aace17db7e2b0e971228bbd978707eb85a6d60c8
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-22 21:02:07 -0500

    add more units of measurement: kilobytes and megabytes

--- commands/system/largest-ram-process.sh
+++ commands/system/largest-ram-process.sh
@@ -12,9 +12,29 @@
 # @raycast.icon 📈
 
 output=$(ps -exco rss,comm | sort -k 1 -n -r | head -n 1)
-bytes=$(echo "$output" | awk -F " " '{ st = index($0," ");print $1}')
+kbytes=$(echo "$output" | awk -F " " '{ st = index($0," ");print $1}')
 cmd=$(echo "$output" | awk -F " " '{ st = index($0," ");print substr($0,st+1)}')
 
-gigabytes=$(echo "scale=3; $bytes / 1024 / 1024" | bc -l)
 
-echo "${cmd} - ${gigabytes}GB"
+## Kilobytes
+print_as_kb=$(echo "$kbytes < 1024" | bc -l)
+
+if (( print_as_kb )); then
+	echo "${cmd} - ${kbytes} KB"
+	exit 0
+fi
+
+
+## Megabytes
+mbytes=$(echo "scale=3; $kbytes / 1024" | bc -l)
+print_as_mb=$(echo "$mbytes < 1024" | bc -l)
+
+if (( print_as_mb )); then
+	echo "${cmd} - ${mbytes} MB"
+	exit 0
+fi
+
+
+## Gigabytes
+gbytes=$( echo "scale=3; $mbytes / 1024" | bc -l)
+echo "${cmd} - ${gbytes} GB"
