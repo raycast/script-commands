8110a285a483a832699c326f43027e076702539a
Zander Martineau <hi@zander.wtf>
2020-11-24 09:50:06 +0000

    Add Script Command: link unfurl test (unfurler.com)

---
+++ commands/web-searches/unfurl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title unfurl
+# @raycast.mode silent
+# @raycast.author Zander Martineau
+# @raycast.authorURL https://zander.wtf
+
+# Optional parameters:
+# @raycast.icon 📦
+# @raycast.packageName Link unfurl tester
+# @raycast.argument1 { "type": "text", "placeholder": "url" }
+
+open "https://unfurler.com/?url=${1// /%20}"
