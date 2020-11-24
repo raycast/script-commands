39a2804f93db516ef08b93e9fe23a5c93f0aa0b7
Caleb Stauffer <crstauf@users.noreply.github.com>
2020-11-24 12:49:12 -0500

    Add Script Command: search-github (#106)
    * add script command: search-github
    
    * fix to not be cdnjs search
    
    * fix the logo also

---
+++ commands/web-searches/search-github.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Search GitHub
+# @raycast.mode silent
+# @raycast.author Caleb Stauffer
+# @raycast.authorURL https://github.com/crstauf
+# @raycast.description Search GitHub.
+# @raycast.icon images/github-logo.png
+# @raycast.argument1 { "type": "text", "placeholder": "Query" }
+# @raycast.packageName Web Searches
+
+open "https://github.com/search?q=${1// /%20}"
