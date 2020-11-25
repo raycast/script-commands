65af32725962c441b4bc1086e1a3fc5741d4ea4d
mmerle <mattdommerle@gmail.com>
2020-11-24 22:22:10 -0500

    Add scripts for cmus

---
+++ commands/media/cmus/next-track.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Next Track
+# @raycast.mode silent
+#
+# @raycast.icon ⏩
+# @raycast.packageName Cmus
+#
+# @raycast.description Goes forward a track if cmus is running
+# @raycast.author mmerle
+# @raycast.authorURL https://github.com/mmerle
+
+
+cmus-remote --next
