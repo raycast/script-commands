7acef84e95787138d0409e3a239c2ef25c15ec77
Thomas Paul Mann <hi@thomaspaulmann.com>
2020-11-26 18:24:36 +0000

    Slack: Add optional expiration time for status

--- commands/communication/slack/set-slack-status.template.sh
+++ commands/communication/slack/set-slack-status.template.sh
@@ -5,6 +5,11 @@
 # remove `.template.` from the filename and set an API token.
 # Optionally, adjust the status text and emoji as well as 
 # the command title and icon to fit your Slack status.
+#
+# API: https://api.slack.com/methods/users.profile.set
+
+
+# Parameters
 
 # Required parameters:
 # @raycast.schemaVersion 1
@@ -20,6 +25,9 @@
 # @raycast.author Thomas Paul Mann
 # @raycast.authorURL https://github.com/thomaspaulmann
 
+
+# Configuration
+
 # To create a new API token, do the following:
 # 1. Create a new Slack app at https://api.slack.com/authentication/basics
 # 2. Add `users.profile:write` to the user token scopes
@@ -33,10 +41,24 @@ STATUS_TEXT="Coffee"
 # String referencing an emoji enabled for the Slack team
 STATUS_EMOJI=":coffee:"
 
+# Minutes until the the status will expire (Empty string will not expire the status)
+STATUS_EXPIRATION_IN_MINUTES="30"
+
+
+# Main program
+
 if [[ -z "$API_TOKEN" ]]
 then 
   echo "No API token provided"
   exit 1
+fi
+
+if [[ -z "$STATUS_EXPIRATION_IN_MINUTES" ]]
+then
+  STATUS_EXPIRATION=0
+else 
+  CURRENT_UNIX_TIME=$(date +%s)
+  STATUS_EXPIRATION=$(($CURRENT_UNIX_TIME + ($STATUS_EXPIRATION_IN_MINUTES * 60)))
 fi
 
 RESPONSE=$(
@@ -44,7 +66,7 @@ curl \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer $API_TOKEN" \
   --request POST \
-  --data "{ 'profile': { 'status_text': '$STATUS_TEXT', 'status_emoji': '$STATUS_EMOJI' } }" \
+  --data "{ 'profile': { 'status_text': '$STATUS_TEXT', 'status_emoji': '$STATUS_EMOJI', 'status_expiration': $STATUS_EXPIRATION } }" \
   --silent \
   --show-error \
   --fail \
