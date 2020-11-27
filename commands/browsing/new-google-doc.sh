8397ca2a4b75420bb590a2c5e143b2e535b1e3a2
samuelcwl <samuelcwl@gmail.com>
2020-11-26 22:44:19 -0500

    Create a new Google Doc

---
+++ commands/browsing/new-google-doc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title New Google Doc
+# @raycast.mode silent
+
+# Optional parameters:
+
+# @raycast.author Sam Ching
+# raycast.packageName browsing
+# @raycast.authorURL https://github.com/samching
+# @raycast.description Launch a new Google Doc in default GSuite account
+# @raycast.icon 📄
+
+open "http://docs.new/"
