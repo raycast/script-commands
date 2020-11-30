c677d9c3d399120c82719642cbdf194fc2b42093
Caleb Stauffer <develop+github@calebstauffer.com>
2020-11-29 20:42:38 -0500

    add script command: search-netflix

---
+++ commands/web-searches/search-netflix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# @raycast.title Search Netflix
+# @raycast.author Caleb Stauffer
+# @raycast.authorURL https://github.com/crstauf
+# @raycast.description Search Netflix.
+
+# @raycast.icon images/netflix-logo.png
+# @raycast.mode silent
+# @raycast.packageName Web Searches
+# @raycast.schemaVersion 1
+
+# @raycast.argument1 { "type": "text", "placeholder": "Title" }
+
+open "https://www.netflix.com/search?q=${1// /%20}"
