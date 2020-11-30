f964b3dd4d33fa47f299ebd95df33eb83b836328
Thiago Holanda <unnamedd@gmail.com>
2020-11-30 22:14:46 +0100

    Add Brew Services Stop Script Command

---
+++ commands/developer-utils/brew/brew-services-stop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Stop Service
+# @raycast.mode compact
+# @raycast.packageName Brew
+# @raycast.argument1 {"type":"text", "placeholder": "Service Name" }
+#
+# Optional parameters:
+# @raycast.icon 🍺
+# @raycast.currentDirectoryPath ~
+# @raycast.needsConfirmation false
+#
+# Documentation:
+# @raycast.description Stop Service in Brew
+# @raycast.author Thiago Holanda
+# @raycast.authorURL https://twitter.com/tholanda
+
+if ! command -v brew &> /dev/null; then
+  echo "brew command is required (https://brew.sh).";
+  exit 1;
+fi
+
+brew services stop $1
