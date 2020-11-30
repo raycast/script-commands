96b3bf9dc9a4a39e1678a5b00f9c7395d88e05f5
Thiago Holanda <unnamedd@gmail.com>
2020-11-30 22:14:14 +0100

    Add Brew Services List Script Command

---
+++ commands/developer-utils/brew/brew-services-list.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Services List
+# @raycast.mode fullOutput
+# @raycast.packageName Brew
+#
+# Optional parameters:
+# @raycast.icon 🍺
+# @raycast.currentDirectoryPath ~
+# @raycast.needsConfirmation false
+#
+# Documentation:
+# @raycast.description Show Brew Services List
+# @raycast.author Thiago Holanda
+# @raycast.authorURL https://twitter.com/tholanda
+
+if ! command -v brew &> /dev/null; then
+  echo "brew command is required (https://brew.sh).";
+  exit 1;
+fi
+
+brew services list
