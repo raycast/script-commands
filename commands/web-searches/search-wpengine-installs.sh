ba517ffdaf139610001fb7bfd093b581cb1003ef
Caleb Stauffer <develop+github@calebstauffer.com>
2020-12-04 22:42:03 -0500

    add script commands: search-wpengine

---
+++ commands/web-searches/search-wpengine-installs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# @raycast.title Search WP Engine Installs
+# @raycast.author Caleb Stauffer
+# @raycast.authorURL https://github.com/crstauf
+# @raycast.description Search [WP Engine](https://wpengine.com) installs.
+
+# @raycast.icon images/wpengine-logo.png
+# @raycast.mode silent
+# @raycast.packageName Web Searches
+# @raycast.schemaVersion 1
+
+# @raycast.argument1 { "type": "text", "placeholder": "Query" }
+
+open "https://my.wpengine.com/omni_search?q=${1// /%20}"
