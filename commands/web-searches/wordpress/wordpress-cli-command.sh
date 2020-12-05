5db0dd49b6144db20f9936f911c12b59381be89b
Caleb Stauffer <develop+github@calebstauffer.com>
2020-12-04 22:07:52 -0500

    add iconDark for WordPress searches

--- commands/web-searches/wordpress/wordpress-cli-command.sh
+++ commands/web-searches/wordpress/wordpress-cli-command.sh
@@ -6,6 +6,7 @@
 # @raycast.description Open [WordPress CLI command reference](https://developer.wordpress.org/cli/commands/) for specified command.
 
 # @raycast.icon images/wordpress-logo.png
+# @raycast.iconDark images/wordpress-logo-iconDark.png
 # @raycast.mode silent
 # @raycast.packageName WordPress
 # @raycast.schemaVersion 1
