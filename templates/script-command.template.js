2bf404740617c6ba992f2127c9fec72027e8ccdb
traynham <jesse@comm-unity.net>
2020-12-05 21:06:32 +0000

    Added ABOUT THIS TEMPLATE comment.

--- templates/script-command.template.js
+++ templates/script-command.template.js
@@ -5,7 +5,7 @@
 // Dependency: This script requires Nodejs.
 // Install Node: https://nodejs.org/en/download/
 //
-// Duplicate this file and remove ".template." from the filename to get started.
+// Duplicate this file and remove ".template" from the filename to get started.
 // See full documentation here: https://github.com/raycast/script-commands
 //
 // Required parameters:
@@ -23,6 +23,24 @@
 // @raycast.description Write a nice and descriptive summary about your script command here 
 // @raycast.author Your name
 // @raycast.authorURL An URL for one of your social medias
+
+/*
+
+	ABOUT THIS TEMPLATE:
+	
+	This template is meant to be a quick starting point for creating a script command using Nodejs.
+	
+	This template demonstrates the following ideas:
+	
+	* Extracting passed arguments.
+	* Using both required and optional arguments
+	* URI Encoding
+	* Outputting result to Raycast
+	* Opening a url using exec and the unix open command
+	* Use of destructuring
+	* Use of template literals
+
+*/
 
 const { exec } = require('child_process')
 
