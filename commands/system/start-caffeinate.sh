d4b4f7d29ebecbebe60504827dd12d891b212411
maximkrouk <id.maximkrouk@gmail.com>
2020-12-06 16:07:07 +0300

    fix(System): Caffeinate start PR fixes

--- commands/system/start-caffeinate.sh
+++ commands/system/start-caffeinate.sh
@@ -9,14 +9,18 @@
 # @raycast.description Prevents your device from sleepping
 # @raycast.icon ☕️
 # @raycast.packageName System
-# @raycast.argument1 { "type": "text", "placeholder": "Seconds" }
+# @raycast.argument1 { "type": "text", "placeholder": "Seconds", "optional": true }
 
-value=$1
-regex='^[0-9]+$'
-if ! [[ $value =~ $regex ]] ; then
-  echo "Provide a number, please  ❌"
-else
-  caffeinate -u -t $1 &
-  echo "Won't sleep for" $value "seconds"
+if [ -z $1 ]
+then
+    caffeinate -u &
+    echo "Won't sleep forever" && exit 0
 fi
 
+regex='^[0-9]+$'
+if ! [[ $1 =~ $regex ]] ; then
+    echo "Provide a number, please" && exit 1
+else
+    caffeinate -u -t $1 &
+    echo "Won't sleep for" $1 "seconds" && exit 0
+fi
