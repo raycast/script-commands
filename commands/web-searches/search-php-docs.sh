c3bccb3e23fbae76972e758a46f9f6f44b0f7415
Caleb Stauffer <develop+github@calebstauffer.com>
2020-12-07 17:32:35 -0500

    add script command: search-php-docs

---
+++ commands/web-searches/search-php-docs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# @raycast.title Search PHP Docs
+# @raycast.author Caleb Stauffer
+# @raycast.authorURL https://github.com/crstauf
+# @raycast.description Search [PHP official documentation](https://www.php.net/docs.php).
+
+# @raycast.icon images/php-logo.png
+# @raycast.iconDark images/php-logo-iconDark.png
+# @raycast.mode silent
+# @raycast.packageName Web Searches
+# @raycast.schemaVersion 1
+
+# @raycast.argument1 { "type": "text", "placeholder": "Query" }
+
+open "https://www.php.net/manual-lookup.php?pattern=${1// /%20}&scope=quickref"
