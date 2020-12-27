870944fd8bc75c3b9b88037e770a96afed6de4e8
JY <ithinkinx7@gmail.com>
2020-12-27 15:43:31 +0800

    Fix Youtube Search Problem
    Signed-off-by: JY <ithinkinx7@gmail.com>

--- commands/web-searches/youtube.sh
+++ commands/web-searches/youtube.sh
@@ -8,6 +8,6 @@
 
 # Optional parameters:
 # @raycast.icon images/youtube.png
-# @raycast.argument1 { "type": "text", "placeholder": "query" }
+# @raycast.argument1 { "type": "text", "placeholder": "query","percentEncoded": true }
 
-open "https://www.youtube.com/results?search_query${1// /%20}"
+open "https://www.youtube.com/results?search_query=${1// /%20}"
