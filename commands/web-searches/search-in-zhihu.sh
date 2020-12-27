10542d31a7d3e1b547c015c102dc75cfd864274d
JY <ithinkinx7@gmail.com>
2020-12-27 11:33:45 +0800

    Add Script Command:Search in Zhihu,Search in Baidu,Bear,Dash
    Signed-off-by: JY <ithinkinx7@gmail.com>

---
+++ commands/web-searches/search-in-zhihu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.author Jax0rz
+# @authorURL https://github.com/Jax0rz
+# @raycast.schemaVersion 1
+# @raycast.title Search in Zhihu
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon images/zhihu.png
+# @raycast.argument1 { "type": "text", "placeholder": "关键词...","percentEncoded": true}
+
+open "https://www.zhihu.com/search?type=content&q=$1"
