6b1e92e008a5af37ba1317a5597914042f944fbc
Jax0rz <ithinkinx7@gmail.com>
2020-12-29 17:21:29 +0800

    Update commands/web-searches/google-search.sh
    Co-authored-by: Thiago Holanda <unnamedd@gmail.com>

--- commands/web-searches/google-search.sh
+++ commands/web-searches/google-search.sh
@@ -10,4 +10,4 @@
 # @raycast.icon images/google.png
 # @raycast.argument1 { "type": "text", "placeholder": "query","percentEncoded": true}
 
-open "https://www.google.com/search?q=${1// /%20}"
+open "https://www.google.com/search?q=$1"
