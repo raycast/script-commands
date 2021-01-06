1fb7d650a1f53210cbd648386e3db93713cca43f
Marc Klingen <marc.klingen@gmail.com>
2021-01-06 16:39:46 +0100

    Add commands to quickly change the default browser

---
+++ commands/browsing/default-browser-chromium.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Dependency: requires defaultbrowser (https://github.com/kerma/defaultbrowser/)
+# Install via Homebrew: `brew install defaultbrowser`
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Switch Default Browser to Chromium
+# @raycast.mode silent
+# @raycast.packageName Browsing
+
+# Optional parameters:
+# @raycast.icon 🧭
+
+# Documentation:
+# @raycast.description Set Chromium as the default browser.
+
+defaultbrowser chromium
+exit 0
---
+++ commands/browsing/default-browser-safari.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Dependency: requires defaultbrowser (https://github.com/kerma/defaultbrowser/)
+# Install via Homebrew: `brew install defaultbrowser`
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Switch Default Browser to Safari
+# @raycast.mode silent
+# @raycast.packageName Browsing
+
+# Optional parameters:
+# @raycast.icon 🧭
+
+# Documentation:
+# @raycast.description Set Safari as the default browser.
+
+defaultbrowser safari
+exit 0
