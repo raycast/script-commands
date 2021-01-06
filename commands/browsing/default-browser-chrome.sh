413f9e7162003e34309b0a271f7ed9c8c89db6e1
Marc Klingen <marc.klingen@gmail.com>
2021-01-06 17:37:32 +0100

    Add author for docs

--- commands/browsing/default-browser-chrome.sh
+++ commands/browsing/default-browser-chrome.sh
@@ -13,6 +13,8 @@
 # @raycast.icon 🧭
 
 # Documentation:
+# @raycast.author Marc Klingen
+# @raycast.authorURL https://github.com/marcklingen
 # @raycast.description Set Chrome as the default browser.
 
 defaultbrowser chrome
