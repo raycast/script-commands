c40a5242c843dcb0cd34c5c79b287d776952b242
Marc Klingen <marc.klingen@gmail.com>
2021-01-13 00:41:47 +0100

    Check whether defaultbrowser is installed

--- commands/system/default-browser-firefox.sh
+++ commands/system/default-browser-firefox.sh
@@ -17,5 +17,10 @@
 # @raycast.authorURL https://github.com/marcklingen
 # @raycast.description Set Firefox as the default browser.
 
+if ! command -v defaultbrowser &> /dev/null; then
+      echo "defaultbrowser is required (https://github.com/kerma/defaultbrowser/).";
+      exit 1;
+fi
+
 defaultbrowser firefox
 exit 0
