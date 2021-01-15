10cbcc9190a0f6d2aacf0a7f160ada59cadccd0f
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-01-15 11:44:56 +0530

    Add rick-roll-me script

---
+++ commands/browsing/rick-roll-me.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Rick Roll Me
+# @raycast.mode silent
+# @raycast.icon images/rick-astley.png
+# @raycast.author Khushraj Rathod
+# @raycast.authorURL https://www.khushrajrathod.com
+# @raycast.description Rick rolls you.
+
+open "https://www.youtube.com/watch?v=dQw4w9WgXcQ%2F"
