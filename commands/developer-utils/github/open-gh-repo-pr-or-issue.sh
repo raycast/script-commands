d09abe05997439d56dda5762d5632f9aad33f98d
Phil Salant <PSalant@gmail.com>
2021-01-18 17:35:59 -0500

    Add a script to open a GH repo, PR, or issue

---
+++ commands/developer-utils/github/open-gh-repo-pr-or-issue.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title GitHub Repository PRs & Issues
+# @raycast.mode compact
+#
+# Optional parameters:
+# @raycast.packageName GitHub
+# @raycast.icon images/github-logo.png
+# @raycast.iconDark images/github-logo-iconDark.png
+# @raycast.argument1 { "type": "text", "placeholder": "Organization/Repository" }
+# @raycast.argument2 { "type": "text", "placeholder": "Pull Request #", "optional": true }
+# @raycast.argument3 { "type": "text", "placeholder": "Issue #", "optional": true }
+#
+# Documentation
+# @raycast.author Phil Salant
+# @raycast.authorURL https://github.com/PSalant726
+# @raycast.description Open a repository, pull request, or issue on GitHub
+
+pull_request=""
+issue=""
+
+if [[ -n "$2" && -n "$3" ]]; then
+  echo "Do not pass both a pull request and an issue."
+  exit 1
+elif [[ -n "$2" ]]; then
+  pull_request="/pull/$2"
+elif [[ -n "$3" ]]; then
+  issue="/issues/$3"
+fi
+
+echo "Opening $1$pull_request$issue on GitHub..."
+open "https://github.com/$1$pull_request$issue"
+exit 0
