dc2005a71a20be0d88207e3e8bbe06a452f67302
Tanay Nistala <nistalatanay@gmail.com>
2021-01-18 10:17:21 +0530

    Updated metadata
    Updated the metadata for each script with description and author

--- commands/apps/bear/bear-open-note.sh
+++ commands/apps/bear/bear-open-note.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Raycast Script Command Template
+# @raycast.title Open Note
+# @raycast.author Tanay Nistala
+# @raycast.authorURL https://github.com/tanaynistala
+# @raycast.description Open the specified note in Bear.
 #
-# Required parameters:
-# @raycast.schemaVersion 1
-# @raycast.title Open Bear Note
-# @raycast.mode silent
-#
-# Optional parameters:
 # @raycast.icon images/bear-light.png
 # @raycast.iconDark images/bear-dark.png
-# @raycast.currentDirectoryPath ~
+#
+# @raycast.mode silent
 # @raycast.packageName Bear
+# @raycast.schemaVersion 1
 # @raycast.argument1 { "type": "text", "placeholder": "Title" }
 
 open "bear://x-callback-url/open-note?title=${1// /%20}"
