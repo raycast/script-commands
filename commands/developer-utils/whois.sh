b07a74ee0c929129fe2c5a71a25283cbe5402bbd
Ronan Nunes <ronan@uber.com>
2021-01-18 18:03:07 -0300

    Update dependency instruction

--- commands/developer-utils/whois.sh
+++ commands/developer-utils/whois.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script requires `whois` CLI which is already installed on macOS. But if for some reason you don't have: brew install whois
+
 # @raycast.title Whois
 # @raycast.author Caleb Stauffer
 # @raycast.authorURL https://github.com/crstauf
@@ -12,7 +14,7 @@
 
 # @raycast.argument1 { "type": "text", "placeholder": "URL" }
 
-if type "whois" &> /dev/null; then
+if command -v whois &> /dev/null; then
     whois $1
 else
     open https://who.is/whois/$1
