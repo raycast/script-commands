d9811978e4ec09fbd5779ee46116c1d40b09ca91
Phil Salant <PSalant@gmail.com>
2021-01-23 18:06:29 -0500

    Use a single `Pull Request or Issue #` argument
    The revelation that the URL structure:
    `github.com/organization/repository/pull/{ issue_number }`
    will automatically redirect to the issue allows for a dramatic
    simplification of this script command. Only a single argument is
    necessary. Additionally, without the need for error handling, the script
    can now run in `silent` mode.

--- commands/developer-utils/github/open-gh-repo-pr-or-issue.sh
+++ commands/developer-utils/github/open-gh-repo-pr-or-issue.sh
@@ -3,15 +3,14 @@
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title Visit a Repository, Pull Request, or Issue
-# @raycast.mode compact
+# @raycast.mode silent
 #
 # Optional parameters:
 # @raycast.packageName GitHub
 # @raycast.icon images/github-logo.png
 # @raycast.iconDark images/github-logo-iconDark.png
 # @raycast.argument1 { "type": "text", "placeholder": "Organization/Repository" }
-# @raycast.argument2 { "type": "text", "placeholder": "Pull Request #", "optional": true }
-# @raycast.argument3 { "type": "text", "placeholder": "Issue #", "optional": true }
+# @raycast.argument2 { "type": "text", "placeholder": "Pull Request or Issue #", "optional": true }
 #
 # Documentation
 # @raycast.author Phil Salant
@@ -19,15 +18,8 @@
 # @raycast.description Open a repository, pull request, or issue on GitHub
 
 path=""
-if [[ -n "$2" && -n "$3" ]]; then
-  echo "Do not pass both a pull request and an issue."
-  exit 1
-elif [[ -n "$2" ]]; then
+if [ -n "$2" ]; then
   path="/pull/$2"
-elif [[ -n "$3" ]]; then
-  path="/issues/$3"
 fi
 
-echo "Opening $1$path on GitHub..."
 open "https://github.com/$1$path"
-exit 0
