e25c4dc6d318b7dd611ea4c711819b4e97717c0f
Tanay Nistala <nistalatanay@gmail.com>
2021-01-24 10:14:41 +0530

    Apply suggestions from code review
    Co-authored-by: Thiago Holanda <unnamedd@gmail.com>

--- commands/apps/bear/bear-search.sh
+++ commands/apps/bear/bear-search.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# @raycast.title Search Bear
+# @raycast.title Search
 # @raycast.author Tanay Nistala
 # @raycast.authorURL https://github.com/tanaynistala
 # @raycast.description Search notes by keyword and/or tag in Bear.
@@ -11,7 +11,7 @@
 # @raycast.mode silent
 # @raycast.packageName Bear
 # @raycast.schemaVersion 1
-# @raycast.argument1 { "type": "text", "placeholder": "Term", "optional": true }
-# @raycast.argument2 { "type": "text", "placeholder": "Tag", "optional": true }
+# @raycast.argument1 { "type": "text", "placeholder": "Term", "optional": true, "percentEncoded": true}
+# @raycast.argument2 { "type": "text", "placeholder": "Tag", "optional": true, "percentEncoded": true}
 
-open "bear://x-callback-url/search?term=${1// /%20}&tag=${2// /%20}"
+open "bear://x-callback-url/search?term=${1}&tag=${2}"
