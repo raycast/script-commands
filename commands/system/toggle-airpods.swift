8618867137e8e4ddf5ab79bdbe2ec586b5f1a9f7
Nichlas Wærnes Andersen <nichlas@itsnwa.com>
2021-01-27 15:25:23 +0100

    added toggle airpods script

---
+++ commands/system/toggle-airpods.swift
@@ -0,0 +1,39 @@
+#!/usr/bin/swift
+
+// Required parameters:
+// @raycast.schemaVersion 1
+// @raycast.title Toggle AirPods
+// @raycast.mode silent
+// @raycast.packageName Audio
+//
+// Optional parameters:
+// @raycast.icon images/airpod.png
+//
+// Documentation:
+// @raycast.description Toggle AirPods bluetooth device
+// @raycast.author Nichlas W. Andersen
+// @raycast.authorURL https://github.com/itsnwa
+
+import IOBluetooth
+
+func toggleAirPods() {
+    guard let bluetoothDevice = IOBluetoothDevice(addressString: "Your AirPods MAC address") else {
+        print("Device not found")
+        exit(-2)
+    }
+
+    if !bluetoothDevice.isPaired() {
+        print("Device not paired")
+        exit(-4)
+    }
+
+    if bluetoothDevice.isConnected() {
+        print("AirPods Disconnected")
+        bluetoothDevice.closeConnection()
+    } else {
+        print("AirPods Connected")
+        bluetoothDevice.openConnection()
+    }
+}
+
+toggleAirPods()
