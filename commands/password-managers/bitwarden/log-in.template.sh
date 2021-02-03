993e37b7d3ad4861725bd7a91225c6d83b9849c3
Phil Salant <PSalant726@users.noreply.github.com>
2021-02-03 02:57:58 -0500

    Add a Bitwarden package (#214)
    * Add Bitwarden package, image, and `Log In` command
    
    The `password-managers` package category has been added, because I
    didn't see another category that felt appropriate.
    
    Logging in authenticates with the Bitwarden server and saves a session
    token to the user's default keychain. This enables the `Log In` script
    command to also unlock a user's vault. For more information on the
    Bitwarden CLI's authentication pattern, see:
    https://bitwarden.com/help/article/cli/#session-management
    
    * `Log In` script is `silent`, prettier documentation
    
    * Add `Vault Status` script
    
    * Add `Lock Vault` script
    
    * Explicitly `unset` variables containing session tokens
    
    This might improve the overall security of the package. If it doesn't,
    it certainly doesn't hurt ¯\_(ツ)_/¯
    
    * Add `Unlock` script
    
    * Add `Log Out` script
    
    * Better variable names and clearer spacing
    
    * Add `Search Vault Items` script
    
    * Add `Copy First Password` script
    
    * Add `README.md`, supporting images
    
    * Align script names with image names and documentation
    
    * Provide a better error message if dependencies are missing
    
    * Rename 'Copy First Password' command to 'Copy First Matching Password'
    
    * Allow copying of a first matching password when multiple items are found
    
    The `bw get item` command errors if the search query returns multiple
    items. This change enables the intended behavior of copying the password
    of the first matching item to the clipboard, by using `jq` to limit the
    search results to the first item instead of the Bitwarden CLI.
    
    Also explicitly `unset`s the `$password` value after is has been copied.
    
    * Improve authentication and session token documentation
    
    * Actually say what the `Log In` command does
    
    * Omit hidden fields in search results by default
    
    Vault items may contain hidden fields. The package should assume that
    these fields contain sensitive data, and should treat them like
    passwords.
    
    * Update screenshot to match new command name
    
    * Ignore useless `type` property of fields
    
    * Use the long form of command args where possible

---
+++ commands/password-managers/bitwarden/log-in.template.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Dependency: This script requires the Bitwarden CLI:
+# https://bitwarden.com/help/article/cli/
+#
+# Install via homebrew: `brew install bitwarden-cli`
+
+# If logging in with multi-factor authentication, set the `MFA_METHOD`
+# as necessary. Otherwise, leave this variable set to the empty string.
+#
+# Authenticator: 0
+# Email: 1
+# Yubikey: 3
+MFA_METHOD=""
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Log In
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.packageName Bitwarden
+# @raycast.icon images/bitwarden.png
+# @raycast.argument1 { "type": "text", "placeholder": "Email" }
+# @raycast.argument2 { "type": "text", "placeholder": "Master Password" }
+# @raycast.argument3 { "type": "text", "placeholder": "MFA Code", "optional": true }
+#
+# Documentation
+# @raycast.author Phil Salant
+# @raycast.authorURL https://github.com/PSalant726
+# @raycast.description Log in to a Bitwarden vault.
+
+if ! command -v bw &> /dev/null; then
+  echo "The Bitwarden CLI is not installed."
+  exit 1
+fi
+
+if [ -n "$MFA_METHOD" ]; then
+  mfa="--method $MFA_METHOD --code $3"
+fi
+
+out=$(bw --raw login $1 $2 $mfa)
+status=$?
+
+if [ $status -eq 0 ]; then
+  security add-generic-password -U -a ${USER} -s raycast-bitwarden -j "Bitwarden session token for use with Raycast" -w $out
+  unset $out
+  echo "Login successful! Your vault is now unlocked."
+  exit 0
+else
+  echo $out
+  exit 1
+fi
