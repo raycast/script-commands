6b94182f236558b261295da85bd2b057ecb79690
ugo-quelhas <ugo.quelhas@axa.fr>
2021-02-12 11:49:03 +0100

    Revert "Color Conversion command"
    This reverts commit 2bb58a62344e9baf3eb3c401231228f24810a741.

---
+++ commands/developer-utils/ip/get-external-ip-v4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title External IPv4
+# @raycast.mode inline
+# @raycast.refreshTime 1h
+# @raycast.packageName Internet
+
+# Optional parameters:
+# @raycast.icon 🌐
+
+# Documentation:
+# @raycast.description Copies the external IPv4 to the clipboard.
+
+ip=$(curl -4 -s -m 5 https://api.ipify.org)
+echo $ip
---
+++ commands/developer-utils/ip/get-external-ip-v6.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title External IPv6
+# @raycast.mode inline
+# @raycast.refreshTime 1h
+# @raycast.packageName Internet
+
+# Optional parameters:
+# @raycast.icon 🌐
+
+# Documentation:
+# @raycast.description Copies the external IPv6 to the clipboard.
+
+ip=$(curl -6 -s -m 5 https://api64.ipify.org)
+echo $ip
