86500df052d26bc57940ee7064d35f8c4f654356
Thomas Paul Mann <hi@thomaspaulmann.com>
2021-02-16 22:31:57 +0000

    Dev Utils: Add script to create image of code

---
+++ commands/developer-utils/create-image-from-code.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Create Image from Code
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ./images/ray-so.png
+# @raycast.argument1 { "type": "text", "placeholder": "Title", "optional": true }
+
+# Documentation:
+# @raycast.description Create beautiful images of your code with https://ray.so
+# @raycast.author Thomas Paul Mann
+# @raycast.authorURL https://github.com/thomaspaulmann
+
+# Customization:
+# Set colors. Available options: candy, breeze, midnight or sunset
+COLORS="midnight" 
+# Toggle background. Available options: true or false
+BACKGROUND="true"
+# Toggle dark mode. Available options: true or false
+DARK_MODE="true"
+# Set padding. Available options: 16, 32, 64 or 128
+PADDING="64"
+
+# Main:
+
+if [[ -z "$1" ]]
+then 
+  TITLE="Untitled-1"
+else
+  TITLE=$1
+fi
+
+CODE=$(pbpaste | base64)
+
+open "https://ray.so/?colors=$COLORS&background=$BACKGROUND&darkMode=$DARK_MODE&padding=$PADDING&title=$TITLE&code=$CODE"
