2298f8f8a4f0de536e499d102e1daeb5687b00d8
Sam Stephenson <sam@stephenson.net>
2021-02-19 13:42:28 +0000

    re-add Thiago’s changes

--- commands/google-maps/google-maps.sh
+++ commands/google-maps/google-maps.sh
@@ -11,10 +11,13 @@
 # @raycast.argument1 { "type": "text", "placeholder": "Location", "optional": true}
 # @raycast.argument2 { "type": "text", "placeholder": "Destination", "optional": true}
 
+first_argument=${1// /+}
+second_argument=${2// /+}
+
 if [$1 == ""]; then
 	open "https://www.google.com/maps"
 elif [$2 == ""]; then
-	open "https://www.google.com/maps/search/${1// /+}"
+	open "https://www.google.com/maps/search/$first_argument"
 else
-	open "https://www.google.com/maps/dir/${2// /+}/${1// /+}"
+	open "https://www.google.com/maps/dir/$second_argument/$first_argument"
 fi
