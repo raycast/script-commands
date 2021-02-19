bd8a2a8b3c6cc8758f4a05946b7e125f111cff0e
Aleksei Zotov <aleksei.zotov@getbux.com>
2021-02-19 21:01:03 +0100

    Add a brew install script

---
+++ commands/developer-utils/brew/brew-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Install a package
+# @raycast.mode fullOutput
+# @raycast.packageName Brew
+#
+# Optional parameters:
+# @raycast.icon 🍺
+# @raycast.needsConfirmation true
+# @raycast.argument1 {"type": "text", "placeholder": "Package name"}
+#
+# Documentation:
+# @raycast.description Installs specified brew package.
+# @raycast.author Alex Zotov
+# @raycast.authorURL https://github.com/lex4hex
+
+if ! command -v brew &> /dev/null; then
+  echo "brew command is required (https://brew.sh).";
+  exit 1;
+fi
+
+brew install {$1}
+
