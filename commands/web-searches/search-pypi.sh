fbc48d658efeb2999e4477eaff21501771a6d625
Lucas Costa <lucasrcosta@gmail.com>
2021-02-20 16:54:22 -0800

    Add Python documentation and package searches

---
+++ commands/web-searches/search-pypi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# @raycast.title Search Python Package Index (PyPI)
+# @raycast.author Lucas Costa
+# @raycast.authorURL https://github.com/lucasrcosta
+# @raycast.description Search Python Package Index (PyPI)
+
+# @raycast.icon images/pip.png
+# @raycast.mode silent
+# @raycast.packageName Web Searches
+# @raycast.schemaVersion 1
+
+# @raycast.argument1 { "type": "text", "placeholder": "Title" }
+
+open "https://pypi.org/search/?q=${1// /+}"
