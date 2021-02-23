9ef2b14b95c7f0026f3f42bfb97c66d4ab7ce178
Francois LP <francois@remazing.eu>
2021-02-23 20:59:22 +0100

    feat: add undraw to web searches

---
+++ commands/web-searches/undraw.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Search in Undraw
+# @raycast.mode silent
+# @raycast.packageName Web Searches
+
+# Optional parameters:
+# @raycast.icon images/undraw.png
+
+open "https://undraw.co/search"
