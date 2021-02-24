649678daef023ad39ce3a33dcbcb23c2f4f5879a
Brandon Escamilla <brandon.escamilla@outlook.com>
2021-02-24 14:05:17 -0600

    Create a new email from Raycast using RFC 2368 syntax convention

---
+++ commands/system/new-email.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title New Email
+# @raycast.mode silent
+# @raycast.author Brandon Escamilla
+# @raycast.authorURL https://github.com/brandonescamilla
+# Optional parameters:
+# @raycast.icon 📧
+# @raycast.packageName System
+# @raycast.argument1 { "type": "text", "placeholder": "To:", "optional": false, "percentEncoded": true }
+# @raycast.argument2 { "type": "text", "placeholder": "Subject", "optional": true, "percentEncoded": true }
+# @raycast.argument3 { "type": "text", "placeholder": "Body", "optional": false, "percentEncoded": true }
+
+# Documentation:
+# @raycast.description Opens default mail application, and creates a new email with the inputs.
+
+
+open "mailto:${1}?subject=${2}&body=${3}"
+echo "Done"
