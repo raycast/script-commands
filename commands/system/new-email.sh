37955de37ca6f3e3b463dd65980fdf00cccbcf04
Thomas Paul Mann <hi@thomaspaulmann.com>
2021-02-25 10:46:01 +0000

    Update commands/system/new-email.sh

--- commands/system/new-email.sh
+++ commands/system/new-email.sh
@@ -9,7 +9,7 @@
 # Optional parameters:
 # @raycast.icon 📧
 # @raycast.packageName System
-# @raycast.argument1 { "type": "text", "placeholder": "To:", "optional": true, "percentEncoded": true }
+# @raycast.argument1 { "type": "text", "placeholder": "To", "optional": true, "percentEncoded": true }
 # @raycast.argument2 { "type": "text", "placeholder": "Subject", "optional": true, "percentEncoded": true }
 # @raycast.argument3 { "type": "text", "placeholder": "Body", "optional": true, "percentEncoded": true }
 
