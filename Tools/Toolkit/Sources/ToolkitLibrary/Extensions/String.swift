d25235ac2e8e8ec6df1bb6083b45afe4cf2cd523
Thiago Holanda <unnamedd@gmail.com>
2021-02-28 21:21:29 +0100

    Add method to encrypt String to MD5

--- Tools/Toolkit/Sources/ToolkitLibrary/Extensions/String.swift
+++ Tools/Toolkit/Sources/ToolkitLibrary/Extensions/String.swift
@@ -4,6 +4,11 @@
 //
 
 import Foundation
+import CryptoKit
+
+enum StringError: Error {
+  case convertStringToData
+}
 
 extension String {
   var sanitize: String {
@@ -52,5 +57,20 @@ extension String {
     }
 
     return content
+  }
+  
+  func convertToMD5() throws -> String {
+    guard let data = data(using: .utf8) else {
+      throw StringError.convertStringToData
+    }
+    
+    let digest = Insecure.MD5.hash(data: data)
+    
+    let value = digest.map {
+      String(format: "%02hhx", $0)
+    }
+    .joined()
+    
+    return value
   }
 }
