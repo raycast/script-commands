64561f31b41be6ed1d1c56febbb3c463f7fd841c
Chris Koerner <ChessMess@gmail.com>
2021-03-04 09:55:55 -0500

    Add new web search script dndbeyond
    Adds new web search script for searching on the website DNDBeyond.com

---
+++ commands/web-searches/dndbeyond.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title DnDBeyond.com search
+# @raycast.mode silent
+# @raycast.author Chris Koerner Zeller
+# @raycast.authorURL https://github.com/ChessMess
+
+# Optional parameters:
+# @raycast.icon ❓
+# @raycast.packageName Web searches
+# @raycast.argument1 { "type": "text", "placeholder": "searching for", "percentEncoded": true }
+# @raycast.argument2 { "type": "text", "placeholder": "catagory", "optional": true}
+# @raycast.argument3 { "type": "text", "placeholder": "subcatagory", "optional": true}
+
+case ${2} in
+  'c'|'ch'|'cha'|'char'|'character')
+     c='charcters'
+     f='spells,backgrounds,classes,feats,races'
+     ;;
+  'cl'|'class'|'classes')
+     c='charcters'
+     f='classes'
+     ;;
+  'com'|'compendium')
+     c='compendium'
+     f='actions,compendium,conditions,skills,rules,weapon-property'
+     ;;
+   'e'|'eq'|'equip'|'equipment')
+     c='equipment'
+     f='equipment,magic-items'
+     ;;
+   'f'|'fe'|'ft'|'feat'|'feats')
+      c='charcters'
+      f='feats'
+      ;;
+   'i'|'it'|'ite'|'itm'|'item'|'items')
+      c='items'
+      f='equipment,magic-items'
+      ;;
+    'm'|'mag'|'magic'|'magical')
+      c='items'
+      f='spells,magic-items'
+      ;;
+    'mi'|'magicitems'|'magic items'|'magic-items')
+      c='items'
+      f='magic-items'
+      ;;
+    'r'|'ra'|'rac'|'race'|'races')
+        c='charcters'
+        f='races'
+        ;;
+    'r'|'rule'|'rules')
+        c='rules'
+        f='rules'
+        ;;
+    's'|'sp'|'spell'|'spells')
+      c='spells'
+      f='spells'
+      ;;
+    'sk'|'skill'|'skills')
+        c='skills'
+        f='skills'
+        ;;
+    'v'|'vehicle')
+        c='vehicle'
+        f='vehicle'
+        ;;
+    'w'|'weap'|'weapon')
+      c='equipment'
+      f='equipment,magic-items'
+      ;;
+
+   *)
+      c=${2}
+      f=${3}
+     ;;
+esac
+
+open "https://dndbeyond.com/search?q=${1}&c=${c}&f=${f}"
