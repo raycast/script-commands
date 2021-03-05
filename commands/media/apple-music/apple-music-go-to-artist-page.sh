280498cd69bea0489529fb2e00af292438ece951
Jordi Clement <292306+jordicl@users.noreply.github.com>
2021-03-05 16:50:23 +0100

    go to artist page in apple music

---
+++ commands/media/apple-music/apple-music-go-to-artist-page.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Go to Artist in Apple Music
+# @raycast.mode silent
+# @raycast.packageName Music
+
+# Optional parameters:
+# @raycast.author Jordi Clement
+# @raycast.authorURL https://github.com/jordicl
+# @raycast.description images/apple-music-logo.png
+# @raycast.argument1 { "type": "text", "placeholder": "artist", "percentEncoded": true}
+# @raycast.icon images/apple-music-logo.png
+
+url=$(curl -s https://itunes.apple.com/search\?term=$1\&entity=musicArtist | jq '.results[0].artistLinkUrl' | sed 's/https/itms/g' | sed 's/"//g')
+osascript -e 'on run {myurl}' -e 'open location myurl' -e 'end run' $url
