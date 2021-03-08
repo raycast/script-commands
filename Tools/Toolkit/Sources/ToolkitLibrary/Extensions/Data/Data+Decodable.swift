017a0b572aa0027e981cb4fde55e6513b20b4951
Thiago Holanda <unnamedd@gmail.com>
2021-03-08 21:27:49 +0100

    Move extensions to a proper folder

--- Tools/Toolkit/Sources/ToolkitLibrary/Extensions/Data/Data+Decodable.swift
+++ Tools/Toolkit/Sources/ToolkitLibrary/Extensions/Data/Data+Decodable.swift
@@ -8,6 +8,8 @@ import Foundation
 extension Data {
   func decode<T: Codable>(_ type: T.Type = T.self) throws -> T {
     let decoder = JSONDecoder()
+    decoder.dateDecodingStrategy = .iso8601
+    
     let object = try decoder.decode(type, from: self)
 
     return object
