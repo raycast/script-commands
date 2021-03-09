59dfe86eabe5fd9e3ce620bd9617e1cf81ad7bdb
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2021-03-09 10:09:07 -0800

    Update commands/conversions/column-to-comma.sh
    - Add icon and description
    - Add dependency requirements

--- commands/conversions/column-to-comma.sh
+++ commands/conversions/column-to-comma.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Dependency: requires gnu-sed (also known as gsed) (https://formulae.brew.sh/formula/gnu-sed)
+# Install with Homebrew: `brew install gnu-sed`
+
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title Column to Comma
 # @raycast.mode silent
 # @raycast.packageName Conversions
 
+if ! command -v gsed &> /dev/null; then
+	echo "gsed command is required (https://formulae.brew.sh/formula/gnu-sed).";
+	exit 1;
+fi
+
 clipboard=$(pbpaste)
 
-echo "$clipboard" | gsed ':a;N;$!ba;$s/\n/,/g' | pbcopy 
+echo "$clipboard" | gsed ':a;N;$!ba;$s/\n/,/g' | pbcopy
