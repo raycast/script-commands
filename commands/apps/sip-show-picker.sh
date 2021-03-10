7e9c07406ba6a0e32e5b5e847ce88922ed7897e1
James Lyons <jamesjlyons29@gmail.com>
2021-03-09 18:54:05 -0800

    App: Sip
    Add commands to show sip picker, open contrast checker, check contrast between two hex values, and add hex color to sip history.

---
+++ commands/apps/sip-show-picker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Note: Sip X v2.5.3 required
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Show Color Picker
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ./images/sip.png
+# @raycast.packageName Sip
+# Documentation:
+# @raycast.author Sip
+# @raycast.authorURL https://twitter.com/sip_app/
+# @raycast.description Pick a color value from your screen.
+
+open "sip://picker"
