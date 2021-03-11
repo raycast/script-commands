accd87129256cfc9a35c03c5334b53b5f69708f5
Roland Leth <roland@hey.com>
2021-03-11 14:33:06 +0200

    Script to generate gitignore

---
+++ commands/developer-utils/generate-git-ignore.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Generate a .gitignore file
+# @raycast.mode fullOutput
+# @raycast.packageName Developer Utilities
+
+# Optional parameters:
+# @raycast.icon 🤐
+# @raycast.argument1 { "type": "text", "placeholder": "Technologies (react,node,vscode)", "optional": true }
+
+# Documentation:
+# @raycast.description Generates a .gitignore file via https://gitignore.io
+# @raycast.author Roland Leth
+# @raycast.authorURL https://runtimesharks.com
+
+params="$1"
+
+if [[ -z "$params" ]]
+then
+	params="react,node,vscode"
+fi
+
+result=$(curl -s "https://www.toptal.com/developers/gitignore/api/$params")
+
+echo "$result" | pbcopy
+echo "$result"
