2c0cdd45f59cdfb9962f7d6b6f346b3009a4fe52
fate-7 <32026536+fate-7@users.noreply.github.com>
2021-03-13 09:26:00 +0800

    Update brew-list.sh

--- commands/developer-utils/brew/brew-list.sh
+++ commands/developer-utils/brew/brew-list.sh
@@ -12,7 +12,7 @@
 # Documentation:
 # @raycast.description Show Brew List
 # @raycast.author chengzhiqi
-# @raycast.authorURL https://twitter.com/tholanda
+# @raycast.authorURL https://twitter.com/1872Fate
 
 if ! command -v brew &> /dev/null; then
   echo "brew command is required (https://brew.sh).";
