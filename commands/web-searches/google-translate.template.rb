0fe5dfd93eb91d189dc9985894e1e277e492aabb
Roland Leth <roland@hey.com>
2021-03-15 20:42:25 +0200

    Improved script

--- commands/web-searches/google-translate.template.rb
+++ commands/web-searches/google-translate.template.rb
@@ -9,58 +9,29 @@
 # Optional parameters:
 # @raycast.icon 🌍
 # @raycast.argument1 { "type": "text", "placeholder": "Query" }
+# @raycast.argument2 { "type": "text", "placeholder": "from", "optional": true }
+# @raycast.argument3 { "type": "text", "placeholder": "to", "optional": true }
 
 # Documentation:
 # @raycast.description Translates via Google Translate
 # @raycast.author Roland Leth
 # @raycast.authorURL https://runtimesharks.com
 
-require 'net/http'
-require 'json'
-require 'open-uri'
-
-query = ARGV[0]
-
-# You need to fill in the source language and the target language.
-# By default, if you search for some text, it will perform a `sl>tl` translation.
-# There are three ways of using the command; let's say `sl = en` and `tl = de`:
-# - if you want to translate from `en` to `de`, just write your query;
-# - if you want a different `sl` and `tl`, prefix your query with `!sl>tl`,
-# so from `jp` to `fr` you'd write `!jp>fr query`;
-# - if you want a different source languages, prefix your query with `!sl`,
-# so from `jp` to `de`, you'd write `!jp query`.
-
 # Usually translations are short enough, which is why this uses a `compact` mode,
 # but if you'll be using it for longer translations, change line 6 to `fullOutput`.
 # If you do change to `fullOutput`, you might also want to change the last line
 # of the script to `.join("\n\n")`, so each result appears on its own line.
 
-is_custom = query[0] == "!"
-s = query.split(' ')
-q = s.join(" ")
-sl = "" # Source language
-tl = "" # Target language
+require 'net/http'
+require 'json'
+require 'open-uri'
 
-# Require a ! at the start for anything else than the default sl>tl,
-# so it doesn't perform useless and invalid calls.
-if is_custom and s.count > 1
-    q = s[1..].join(" ")
-
-    # Ignore the "!".
-    langs = s.first[1..-1].split('>')
-
-    if langs.first.length >= 2
-        sl = langs.first
-    end
-
-    if langs.count == 2 and langs[1].length >= 2
-        tl = langs[1]
-    end
-end
+q = ARGV[0]
+sl = ARGV[1] == "" ? "en" : ARGV[1] # Source language
+tl = ARGV[2] == "" ? "ro" : ARGV[2] # Target language
 
 # If the length of the query is less than 2, it won't return anything valid.
-# If it starts with !, then the split has to be 2, otherwise we haven't entered a query yet.
-if (is_custom and s.count <= 1) or q.length < 2
+if q.length < 2
     exit 1
 end
 
