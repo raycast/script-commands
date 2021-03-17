0f5d850314c4b059c20b2fd82c1e76b5879aac37
F Sipiano <67593261+FSipiano@users.noreply.github.com>
2021-03-17 18:12:10 +0000

    Updated location, file names and icons

---
+++ commands/developer-utils/GitHub-open-page.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open PR page
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.iconDark images/github-logo-iconDark.png
+# @raycast.icon images/github-logo.png
+
+open https://github.com/pulls/
