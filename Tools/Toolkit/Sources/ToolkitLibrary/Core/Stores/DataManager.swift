0ac7c9b902da52b1ff93fe4d465ead461b20429e
Thiago Holanda <unnamedd@gmail.com>
2021-03-19 01:32:25 +0100

    Add DataManager

---
+++ Tools/Toolkit/Sources/ToolkitLibrary/Core/Stores/DataManager.swift
@@ -0,0 +1,64 @@
+//
+//  MIT License
+//  Copyright (c) 2020-2021 Raycast. All rights reserved.
+//
+
+import TSCBasic
+
+public final class DataManager {
+  private var total: Int = 0 {
+    // FIXME: Data racing
+    didSet {
+      data.totalScriptCommands = total
+    }
+  }
+  
+  var data = RaycastData()
+  
+  let extensionsFilePath: AbsolutePath
+  let extensionsPath: AbsolutePath
+  
+  let fileSystem: FileSystem
+  
+  var isMetadataEmpty: Bool {
+    data.metadata.isEmpty
+  }
+  
+  var extensionsPathString: String {
+    extensionsPath.pathString
+  }
+  
+  public init(extensionsPath: String, extensionsFilename: String = "") throws {
+    let fileSystem          = TSCBasic.localFileSystem
+    let path                = fileSystem.absolutePath(for: extensionsPath)
+    let extensionsFilePath  = path.appending(RelativePath(extensionsFilename))
+    
+    guard fileSystem.exists(path) else {
+      throw ToolkitError.folderNotFound(path.pathString)
+    }
+    
+    self.fileSystem         = fileSystem
+    self.extensionsPath     = path
+    self.extensionsFilePath = extensionsFilePath
+  }
+  
+  func increaseTotal() {
+    total += 1
+  }
+  
+  func loadContent() {
+    if let byteString = try? fileSystem.readFileContents(extensionsPath) {
+      let data = byteString.contents.data
+      
+      do {
+        self.data = try data.decode()
+      }
+      catch {
+        self.data = RaycastData()
+      }
+    }
+    else {
+      data = RaycastData()
+    }
+  }
+}
