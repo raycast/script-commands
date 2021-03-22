b59af63c6ab6b8bdee4f83213ef71c720e1b0ef8
Adam Zethraeus <adam@zethrae.us>
2021-03-22 14:01:56 -0700

    script to convert RTF clipboard to markdown.
    1. pastes clipboard as html through applescript call
    2. processes output with perl for pandoc compatibility
    3. uses pandoc to convert html to markdown
    4. puts result back on clipboard

---
+++ commands/system/rich-text-clipboard-to-markdown.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# @raycast.title rt2md
+# @raycast.author Adam Zethraeus
+# @raycast.authorURL https://github.com/adam-zethraeus
+# @raycast.description Convert rich text clipboard data to markdown using pandoc
+#
+# @raycast.icon 📝
+#
+# @raycast.mode silent
+# @raycast.packageName System
+# @raycast.schemaVersion 1
+
+export LC_CTYPE=UTF-8
+osascript -e 'the clipboard as "HTML"'| perl -ne 'print chr foreach unpack("C*",pack("H*",substr($_,11,-3)))' | pandoc --from=html --to=markdown | pbcopy
