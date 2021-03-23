11e62c00b94cb47e33bfa6b7a30ad048fde9e445
Adam Zethraeus <adam@zethrae.us>
2021-03-23 10:46:06 -0700

    docs & formatting for raycast

--- commands/system/rich-text-clipboard-to-markdown.sh
+++ commands/system/rich-text-clipboard-to-markdown.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# @raycast.title rtf2md
+# @raycast.title Rich Text to Markdown
 # @raycast.author Adam Zethraeus
 # @raycast.authorURL https://github.com/adam-zethraeus
-# @raycast.description Convert rich text clipboard data to github flavoured markdown using pandoc
+# @raycast.description Convert rich text clipboard data to GitHub Flavoured Markdown using Pandoc
 #
 # @raycast.icon 📝
 #
 # @raycast.mode silent
 # @raycast.packageName System
 # @raycast.schemaVersion 1
+
+# Dependency: This script requires the `pandoc` cli to be installed: https://pandoc.org/
+# Install via homebrew: `brew install pandoc`
+
+if ! command -v pandoc &> /dev/null; then
+      echo "pandoc is required (https://pandoc.org/).";
+      exit 1;
+fi
 
 export LC_CTYPE=UTF-8
 osascript -e 'the clipboard as "HTML"'| perl -ne 'print chr foreach unpack("C*",pack("H*",substr($_,11,-3)))' | pandoc --from=html --to=gfm | pbcopy
