6bddf0ae30f1aec8c63c6e48e3d04680c18d1c35
Mujib Azizi <mujib@fixico.nl>
2021-03-24 09:54:16 +0100

    Added sleep 1 to allow for the browser to open and changed MAX_TRIES to 10.  Detect the browser and use the appropriate script for each Browser (there is no Firefox support) to fetch the URL.

--- commands/communication/google-meet/meet.sh
+++ commands/communication/google-meet/meet.sh
@@ -11,12 +11,28 @@
 
 open https://meet.new
 
-MAX_TRIES=20
+MAX_TRIES=10
 TRIES=0
 
+DEFAULT_BROWSER=$(defaults read ~/Library/Preferences/com.apple.LaunchServices/com.apple.launchservices.secure.plist | awk -F'\"' '/http;/{print window[(NR)-1]}{window[NR]=$2}')
+
+get_url () {
+  if [ "$DEFAULT_BROWSER" = "com.google.chrome" ]; then
+    URL=$(osascript -e 'tell application "Chrome" to URL of active tab of front window as text')
+  elif [ "$DEFAULT_BROWSER" = "com.apple.safari" ]; then
+    URL=$(osascript -e 'tell application "Safari" to return URL of front document')
+  elif [ "$DEFAULT_BROWSER" = "org.mozilla.firefox" ]; then
+    URL=$(osascript -e 'tell applcation "Firefox" to activate')
+    echo "There is no support for Firefox yet. Please copy the URL manually"
+    exit 0
+  fi
+}
 
 while true; do
   ((TRIES++))
+
+  # Allow some time between each iteration to perform an attempt
+  sleep 1
 
   # As we're doing `while true`, we do want to make sure we can
   # exit the script when we feel we've tried too many times.
@@ -25,7 +41,9 @@ while true; do
     break;
   fi
 
-  URL=$(osascript -e 'tell application "Chrome" to URL of active tab of first window as text')
+  get_url
+
+  echo $URL
 
   # First, we'll check if we are still on the Google Meet page.
   if [[ $URL != "https://meet.google.com"* ]]; then
