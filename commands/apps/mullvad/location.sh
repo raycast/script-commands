a5a32b7e537517826704879956c25df7fd90df10
Phil Salant <PSalant726@users.noreply.github.com>
2021-03-25 18:08:20 -0400

    Add a Mullvad package (#282)
    * Add a `connect` script, Mullvad logo
    
    * Add a `disconnect` script
    
    * Add a `connect to location` script
    
    * Add a `reconnect` script
    
    * Add a `status` script
    
    * Add an `entity search` script
    
    * Minor tweaks
    
    - `disconnect.sh`: Don't explicitly `exit 0` when not needed
    - `reconnect.sh`: Grammar fix to description
    - `search.sh`: Don't print usage instructions when no results are found
    
    * Improve messaging when attempting to reconnect while disconnected
    
    * Add documentation and relevant screenshots

---
+++ commands/apps/mullvad/location.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Connect to Location
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.packageName Mullvad
+# @raycast.icon images/mullvad.png
+# @raycast.argument1 { "type": "text", "placeholder": "Country (2-letter code)" }
+# @raycast.argument2 { "type": "text", "placeholder": "City (3-letter code)", "optional": true }
+# @raycast.argument3 { "type": "text", "placeholder": "Hostname", "optional": true }
+#
+# Documentation:
+# @raycast.author Phil Salant
+# @raycast.authorURL https://github.com/PSalant726
+# @raycast.description Connect the Mullvad VPN tunnel using the specified location.
+#
+# Dependencies:
+# The Mullvad CLI: https://mullvad.net/en/help/cli-command-wg/
+
+if ! command -v mullvad &> /dev/null; then
+  echo "The Mullvad CLI is not installed"
+  exit 1
+fi
+
+location=$(mullvad relay set location "$@" 2>&1)
+location_set=$?
+
+if [ $location_set -ne 0 ]; then
+  echo "${location//error: /}"
+  exit 1
+fi
+
+mullvad connect --wait
+
+success=$?
+if [ $success -ne 0 ]; then
+  echo "Failed to establish a VPN tunnel"
+  exit 1
+fi
+
+status=$(mullvad status --location)
+relay=$(echo "$status" | sed -n '2 s/Relay: //p')
+location=$(echo "$status" | sed -n '5 s/Location: //p')
+echo "Successfully connected to $relay ($location)"
---
+++ commands/apps/mullvad/status.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Mullvad Status
+# @raycast.mode inline
+#
+# Conditional parameters:
+# @raycast.refreshTime 5m
+#
+# Optional parameters:
+# @raycast.packageName Mullvad
+# @raycast.icon images/mullvad.png
+#
+# Documentation:
+# @raycast.author Phil Salant
+# @raycast.authorURL https://github.com/PSalant726
+# @raycast.description Display the current status of the Mullvad VPN tunnel connection.
+#
+# Dependencies:
+# The Mullvad CLI: https://mullvad.net/en/help/cli-command-wg/
+
+if ! command -v mullvad &> /dev/null; then
+  echo "⚠️  The Mullvad CLI is not installed"
+  exit 1
+fi
+
+status=$(mullvad status --location)
+
+success=$?
+if [ $success -ne 0 ]; then
+  echo "⚠️  Failed to update VPN tunnel connection status"
+  exit 1
+fi
+
+if [[ $status == *"Tunnel status: Disconnected"* ]]; then
+  echo "❌  Disconnected and unsecured"
+else
+  relay=$(echo "$status" | sed -n '2 s/Relay: //p')
+  location=$(echo "$status" | sed -n '5 s/Location: //p')
+  echo "✅  Connected to $relay ($location)"
+fi
