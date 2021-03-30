30f5c9654aa7ba1951bb96be409ee376f8a0412c
Mujib Azizi <MujibAzizi@users.noreply.github.com>
2021-03-30 10:13:53 +0200

    Added script to create a new Google Meet meeting and copy the url  (#235)
    * Added script to create a new Google Meet meeting and copy the url directly. Fails after 20 tries.
    
    * Move to the correct directory
    
    * Removed a leftover when I was testing
    
    * Added sleep 1 to allow for the browser to open and changed MAX_TRIES to 10.  Detect the browser and use the appropriate script for each Browser (there is no Firefox support) to fetch the URL.
    
    * Remove debugging
    
    * Add notice for unsupported browsers
    
    * Remove unsupported browser notice
    
    * Remove debugging code
    
    * Rename to `Open Google Meet`. Thank you @fe9lix!

---
+++ commands/communication/google-meet/meet.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title Open Google Meet
+# @raycast.mode silent
+# @raycast.packageName Google
+# @raycast.icon images/logo.png
+# @raycast.author Mujib Azizi
+# @raycast.authorURL https://github.com/mujibazizi
+# @raycast.description Start a Google Meet session
+
+open https://meet.new
+
+MAX_TRIES=10
+TRIES=0
+
+DEFAULT_BROWSER=$(defaults read ~/Library/Preferences/com.apple.LaunchServices/com.apple.launchservices.secure.plist | awk -F'\"' '/http;/{print window[(NR)-1]}{window[NR]=$2}')
+
+get_url () {
+  if [ "$DEFAULT_BROWSER" = "com.google.chrome" ]; then
+    URL=$(osascript -e 'tell application "Chrome" to URL of active tab of front window as text')
+  elif [ "$DEFAULT_BROWSER" = "com.apple.safari" ]; then
+    URL=$(osascript -e 'tell application "Safari" to return URL of front document')
+  elif [ "$DEFAULT_BROWSER" = "org.mozilla.firefox" ]; then
+    URL=$(osascript -e 'tell applcation "Firefox" to activate')
+    echo "There is no support for Firefox yet. Please copy the URL manually"
+    exit 0
+  fi
+}
+
+while true; do
+  ((TRIES++))
+
+  # Allow some time between each iteration to perform an attempt
+  sleep 1
+
+  # As we're doing `while true`, we do want to make sure we can
+  # exit the script when we feel we've tried too many times.
+  if [[ "$TRIES" -gt "$MAX_TRIES" ]]; then
+    echo "Could not copy Google Meet url"
+    break;
+  fi
+
+  get_url
+
+  # First, we'll check if we are still on the Google Meet page.
+  if [[ $URL != "https://meet.google.com"* ]]; then
+    continue
+  fi
+
+  # Next, we want to make sure it's not still loading.
+  if [[ $URL == *"new"* ]]; then
+    continue
+  fi
+
+  echo $URL | pbcopy
+  echo "Copied Google Meet url"
+  break
+done
