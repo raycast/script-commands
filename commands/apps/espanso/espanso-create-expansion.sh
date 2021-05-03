b739fb59b463e8f87aea18624962831975ce0a11
Max Stoiber <contact@mxstbr.com>
2021-05-03 09:58:45 +0200

    Handle missing dependency

--- commands/apps/espanso/espanso-create-expansion.sh
+++ commands/apps/espanso/espanso-create-expansion.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Dependency: This script requires `espanso` cli installed: https://espanso.org/install/
+
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title Create Text Expansion
@@ -13,6 +15,11 @@
 # @raycast.description Add a text expansion to expanso
 # @raycast.author Max Stoiber
 # @raycast.authorURL https://github.com/mxstbr
+
+if ! command -v espanso &> /dev/null; then
+      echo "espanso is required (https://espanso.org/install).";
+      exit 1;
+fi
 
 echo "  - trigger: \"$1\"
     replace: \"$2\"" >> ~/Library/Preferences/espanso/default.yml
