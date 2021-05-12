cb557286099b3dcce5deb25191c31e2afe3c5121
es183923 <72744903+es183923@users.noreply.github.com>
2021-05-11 20:44:52 -0400

    Add espanso files

---
+++ commands/apps/espanso/disable-espanso.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Disable Espanso
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon images/espanso.png
+# @raycast.packageName Espanso
+
+# Documentation:
+# @raycast.author es183923
+# @raycast.authorURL https://github.com/es183923
+
+if ! command -v espanso &> /dev/null; then
+      echo "espanso is required (https://espanso.org/install).";
+      exit 1;
+fi
+
+espanso cmd disable
