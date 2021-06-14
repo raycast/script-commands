f7512e6df19bd9825a3c6791a611324511e6daf8
Kirill Gorbachyonok <cool45akol@gmail.com>
2021-06-14 20:33:34 +0300

    feat: clear xcode related junk script (#463)
    * feat: clear xcode related junk script
    
    * fix: paths globing
    
    * feat: add carthage cache clear

---
+++ commands/developer-utils/clear_xcode.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Clear All Related Junk
+# @raycast.mode compact
+# @raycast.packageName Xcode
+#
+# Optional parameters:
+# @raycast.icon 🧹
+#
+# Documentation:
+# @raycast.description This script clears all Xcode related junk.
+# @raycast.author Kirill Gorbachyonok
+# @raycast.authorURL https://github.com/japanese-goblinn
+
+rm -rf ~/Library/Caches/CocoaPods
+rm -rf ~/Library/Caches/org.carthage.CarthageKit
+
+xcrun simctl delete unavailable 
+
+rm -rf ~/Library/Developer/Xcode/Archives
+rm -rf ~/Library/Developer/Xcode/DerivedData
+rm -rf ~/Library/Developer/Xcode/iOS Device Logs
+
+echo "Junk removed!"
