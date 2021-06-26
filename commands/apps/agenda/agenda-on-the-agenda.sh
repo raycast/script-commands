f3e4a99df4b2505f8d7f1249a3f8305e5b3e2639
Michael Ellis <michael.ellis@signalpath.com>
2021-06-26 15:52:43 -0400

    Agenda commands

---
+++ commands/apps/agenda/agenda-on-the-agenda.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Note: Agenda required
+# Install via Mac App Store: https://apps.apple.com/us/app/agenda/id1287445660
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title On the Agenda Overview
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon images/agenda.png
+# @raycast.packageName Agenda
+
+# Documentation:
+# @raycast.description Opens Agenda - On the Agenda Overview
+# @raycast.author Michael Ellis
+# @raycast.authorURL https://github.com/mtellis2
+
+
+open "agenda://x-callback-url/on-the-agenda"
+
+echo "Opened On the Agenda Notes."
