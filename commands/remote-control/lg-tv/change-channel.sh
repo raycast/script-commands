eef1b8c9dd2c06427fcc99a5560eafb835f620b9
Jakub Lanski <jakub.lanski@roche.com>
2021-07-09 02:52:48 +0200

    Add commands for remote control of LG TVs

---
+++ commands/remote-control/lg-tv/change-channel.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Dependencies:
+# LGWebOSRemote: https://github.com/klattimer/LGWebOSRemote
+
+# Recommended installation: 
+# Use pipx (https://github.com/pypa/pipx) to install the package system-wide:
+# pipx install git+https://github.com/klattimer/LGWebOSRemote
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Change Channel
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.packageName LG TV
+# @raycast.icon images/lg.png
+# @raycast.argument1 { "type": "text", "placeholder": "Channel ID" }
+
+# Documentation:
+# @raycast.author Jakub Lanski
+# @raycast.authorURL https://github.com/jaklan
+# @raycast.description Change the TV channel by its ID.
+
+# Modified PATH to include pipx-installed packages. If you used a different installation method, adjust the variable properly to make the 'lgtv' package detectable.
+PATH="$HOME/.local/bin:$PATH"
+
+lgtv tv setTVChannel $1
---
+++ commands/remote-control/lg-tv/turn-off-screen.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Dependencies:
+# LGWebOSRemote: https://github.com/klattimer/LGWebOSRemote
+
+# Recommended installation: 
+# Use pipx (https://github.com/pypa/pipx) to install the package system-wide:
+# pipx install git+https://github.com/klattimer/LGWebOSRemote
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Turn Off Screen
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.packageName LG TV
+# @raycast.icon images/lg.png
+
+# Documentation:
+# @raycast.author Jakub Lanski
+# @raycast.authorURL https://github.com/jaklan
+# @raycast.description Turn off the screen.
+
+# Modified PATH to include pipx-installed packages. If you used a different installation method, adjust the variable properly to make the 'lgtv' package detectable.
+PATH="$HOME/.local/bin:$PATH"
+
+lgtv tv screenOff
