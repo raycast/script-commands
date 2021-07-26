a7ba0bd308aa928acfffb6dedaa28f789e2d70be
pomdtr <achille.lacoin@dailymotion.com>
2021-07-26 17:10:16 +0200

    implement review suggestions

--- commands/apps/kit/import-kit-scripts.py
+++ commands/apps/kit/import-kit-scripts.py
@@ -6,8 +6,8 @@
 # @raycast.mode fullOutput
 
 # Optional parameters:
-# @raycast.icon ./img/kit_logo.png
-# @raycast.packageName Kit
+# @raycast.icon ./images/kit_logo.png
+# @raycast.packageName Script Kit
 
 # Documentation:
 # @raycast.description Convert all Script Kit scripts to Raycast Script Commands
@@ -21,7 +21,10 @@ import sys
 
 db_path = os.path.join(os.path.expanduser("~"), ".kit", "db", "scripts.json")
 if not os.path.exists(db_path):
-    print("Kit is required! Installation Link -> https://www.scriptkit.com/", file=sys.stderr)
+    print(
+        "Kit is required! Installation Link -> https://www.scriptkit.com/",
+        file=sys.stderr,
+    )
     sys.exit(1)
 
 with open(db_path) as f:
@@ -31,8 +34,15 @@ with open("template.txt") as f:
     RAYCAST_SCRIPT_TEMPLATE = string.Template(f.read())
 
 for script in scripts_db["scripts"]:
-    print("Importing {}...".format(script['command']))
-    raycast_script_content = RAYCAST_SCRIPT_TEMPLATE.safe_substitute(script)
+    print("Importing {}...".format(script["command"]))
+    raycast_script_content = RAYCAST_SCRIPT_TEMPLATE.safe_substitute(
+        {
+            "description": script["description"],
+            "title": script["name"],
+            "author": script["author"],
+            "command": script["command"]
+        }
+    )
     output_path = os.path.join("kit_scripts", script["command"] + ".sh")
     with open(output_path, "w") as f:
         f.write(raycast_script_content)
