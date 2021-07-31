e3acf64a9959d84c3d8181392a10bd62ac26b4f3
pomdtr <achille.lacoin@dailymotion.com>
2021-07-31 15:00:40 +0200

    update template filename

--- commands/apps/script-kit/import-scripts-from-script-kit.py
+++ commands/apps/script-kit/import-scripts-from-script-kit.py
@@ -31,7 +31,7 @@ if not os.path.exists(db_path):
 with open(db_path) as f:
     scripts_db = json.load(f)
 
-with open("script.txt") as f:
+with open("template.txt") as f:
     RAYCAST_SCRIPT_TEMPLATE = string.Template(f.read())
 
 for file in glob.glob("scripts/*.sh"):
