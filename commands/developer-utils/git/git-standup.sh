40b62336fa19b00702dd0d08f0455ee55fe29d46
Martin Youhui Shi <186@186.dk>
2021-08-01 01:35:05 +0200

    Update some commands

--- commands/developer-utils/git/git-standup.sh
+++ commands/developer-utils/git/git-standup.sh
@@ -10,8 +10,9 @@
 # Optional parameters:
 # @raycast.icon ./images/git.png
 # @raycast.packageName Git
-# @raycast.argument1 { "type": "text", "placeholder": "Since", "optional": true }
-# @raycast.currentDirectoryPath ~/Developer/script-commands
+# @raycast.argument1 { "type": "text", "placeholder": "Project name"}
+# @raycast.argument2 { "type": "text", "placeholder": "Since (optional; eg. 1 week)", "optional": true }
+
 
 # Documentation:
 # @raycast.author Thomas Paul Mann
@@ -19,10 +20,18 @@
 # @raycast.description Lists your commits from the last 24 hours. Optionally specify since when, e.g. "1 week".
 
 if [ -n "$1" ]; then
-  SINCE="$1"
+  PROJECT="$1"
+else
+  echo "Please given project name in ~/code"
+  exit 1
+fi
+
+if [ -n "$2" ]; then
+  SINCE="$2"
 else
   SINCE="yesterday.midnight"
 fi
+cd ~/code/"$PROJECT"
 
 USER_NAME=$(git config user.name)
 git log --author="$USER_NAME" --since="$SINCE" --oneline --pretty=format:"%s %Cblue(%ar)%Creset" --color
