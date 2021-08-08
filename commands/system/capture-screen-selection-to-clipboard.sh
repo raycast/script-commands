2957cde630570f8c4686c88dc98d0d92cfb47de8
Aaron Miller <aaronmiller@gmail.com>
2021-08-07 19:28:35 -0700

    updating to use  binary

--- commands/system/capture-screen-selection-to-clipboard.sh
+++ commands/system/capture-screen-selection-to-clipboard.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/osascript
+#!/bin/sh
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Capture Screen Selection to Desktop
+# @raycast.title Capture Screen Selection to Clipboard
 # @raycast.mode silent
 # @raycast.packageName System
 #
@@ -10,8 +10,9 @@
 # @raycast.icon 💻
 #
 # Documentation:
-# @raycast.description This script screenshots the selected area and saves it to the desktop.
+# @raycast.description This script screenshots the selected area and saves it to the clipboard.
 # @raycast.author Aaron Miller
 # @raycast.authorURL https://github.com/aaronhmiller
 
-tell application "System Events" to key code 21 using {shift down, command down}
+screencapture -ci
+echo "Selection saved to clipboard"
