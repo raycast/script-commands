fb29d25f1d04a25f5faa95f44f913b9403b66151
Kabilan VS <51330370+KABILAN235@users.noreply.github.com>
2021-08-20 10:38:50 +0530

    Fixed Issues

---
+++ commands/developer-utils/flutter-create.template.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Create Project
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon images/flutter.png
+# @raycast.argument1 { "type": "text", "placeholder": "Project Name" }
+# @raycast.packageName Flutter
+
+# Documentation:
+# @raycast.description Creates a Flutter Project
+# @raycast.author Kabilan VS
+# @raycast.authorURL https://github.com/KABILAN235
+
+# Flutter SDK Path. This Variable Points to the bin Folder in the Flutter SDK.
+SDK_PATH="/Users/$USER/DevTools/flutter/bin"
+
+# Workspace Path. Add the Path To Your Default Workspace, This Will Be The Directory Where your Projects are Created
+WORKSPACE_PATH="/Users/$USER/FlutterProjects"
+
+export PATH="$PATH:$SDK_PATH"
+first_argument=${1// /+}
+cd $WORKSPACE_PATH;
+flutter create $first_argument
+
+
