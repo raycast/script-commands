30958de5d53e6264a67d110b0c8a86d0f9410480
pomdtr <achille.lacoin@dailymotion.com>
2021-09-20 20:41:23 +0200

    add trex integration

---
+++ commands/apps/trex/capture-text.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Capture Text from Selection
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon images/trex.png
+# @raycast.packageName Trex
+
+# Documentation:
+# @raycast.description Capture text from selection using Trex
+# @raycast.author Achille Lacoin
+# @raycast.authorURL https://github.com/pomdtr
+
+open trex://capture
