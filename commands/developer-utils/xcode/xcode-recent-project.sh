b2ce52e7d1b39e738679da3c584ee56649d3f53e
Sergey Korobyin <sergey.korobyin@yellowcat.io>
2021-09-28 19:41:01 +0300

    Fix metadata parameters for Xcode commands

--- commands/developer-utils/xcode/xcode-recent-project.sh
+++ commands/developer-utils/xcode/xcode-recent-project.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# @raycast.title Xcode
-# @raycast.author Sergey Korobyin
-# @raycast.authorURL https://github.com/huangsemao
-# @raycast.description Open recent xcode project
-
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Recent Xcode project
+# @raycast.title Recent Project
 # @raycast.mode silent
-# @raycast.icon images/xcode.png
-# @raycast.packageName Xcode
+# @raycast.packageName Developer Utilities
 # @raycast.argument1 { "type": "text", "placeholder": "Project name", "percentEncoded": true}
+#
+# Optional parameters:
+# @raycast.icon images/xcode.png
+# @raycast.needsConfirmation false
+#
+# Documentation:
+# @raycast.description Open recent Xcode project
+# @raycast.author Sergey Korobyin
+# @raycast.authorURL https://github.com/huangsemao
 
 mdfind "(kMDItemFSName == '*$1*'c) && ((kMDItemContentType =='com.apple.xcode.project') || (kMDItemContentType == 'com.apple.dt.document.workspace') || (kMDItemContentType == 'com.apple.dt.playground'))" -0 | xargs -0 ls -t | head -1 | cut -f1 -d: | awk '{print "\x27" $0 "\x27" }' | xargs  open
