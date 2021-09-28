c1b0a638a3912617a3a82380ffadf2dc08f1b501
Sergey Korobyin <sergey.korobyin@yellowcat.io>
2021-09-28 19:41:01 +0300

    Fix metadata parameters for Xcode commands

--- commands/developer-utils/xcode/xcode-last-project.sh
+++ commands/developer-utils/xcode/xcode-last-project.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# @raycast.title Xcode
-# @raycast.author Sergey Korobyin
-# @raycast.authorURL https://github.com/huangsemao
-# @raycast.description Open the last xcode project
-
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Last Xcode project
+# @raycast.title Last Project
 # @raycast.mode silent
+# @raycast.packageName Developer Utilities
+#
+# Optional parameters:
 # @raycast.icon images/xcode.png
-# @raycast.packageName Xcode
+# @raycast.needsConfirmation false
+#
+# Documentation:
+# @raycast.description Open the last Xcode project 
+# @raycast.author Sergey Korobyin
+# @raycast.authorURL https://github.com/huangsemao
 
 mdfind "(kMDItemContentType =='com.apple.xcode.project') || (kMDItemContentType == 'com.apple.dt.document.workspace') || (kMDItemContentType == 'com.apple.dt.playground')" -0 | xargs -0 ls -t | head -1 | cut -f1 -d: | awk '{print "\x27" $0 "\x27" }' | xargs  open
