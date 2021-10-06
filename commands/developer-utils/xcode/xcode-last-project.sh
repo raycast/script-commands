ff5c52f151d255ff1e39f9d582ac26ded47e9721
Sergey Korobyin <sergey.korobyin@yellowcat.io>
2021-10-06 14:47:41 +0300

    Add scripts to open recent xcode projects

--- commands/developer-utils/xcode/xcode-last-project.sh
+++ commands/developer-utils/xcode/xcode-last-project.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Required parameters:
-# @raycast.schemaVersion 1
-# @raycast.title Last Project
-# @raycast.mode silent
-# @raycast.packageName Developer Utilities
-#
-# Optional parameters:
-# @raycast.icon images/xcode.png
-# @raycast.needsConfirmation false
-#
-# Documentation:
-# @raycast.description Open the last Xcode project 
+# @raycast.title Xcode
 # @raycast.author Sergey Korobyin
 # @raycast.authorURL https://github.com/huangsemao
+# @raycast.description Open the last xcode project
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Last Xcode project
+# @raycast.mode silent
+# @raycast.icon images/xcode.png
+# @raycast.packageName Xcode
 
 mdfind "(kMDItemContentType =='com.apple.xcode.project') || (kMDItemContentType == 'com.apple.dt.document.workspace') || (kMDItemContentType == 'com.apple.dt.playground')" -0 | xargs -0 ls -t | head -1 | cut -f1 -d: | awk '{print "\x27" $0 "\x27" }' | xargs  open
--- commands/developer-utils/xcode/xcode-recent-project.sh
+++ commands/developer-utils/xcode/xcode-recent-project.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Required parameters:
-# @raycast.schemaVersion 1
-# @raycast.title Recent Project
-# @raycast.mode silent
-# @raycast.packageName Developer Utilities
-# @raycast.argument1 { "type": "text", "placeholder": "Project name", "percentEncoded": true}
-#
-# Optional parameters:
-# @raycast.icon images/xcode.png
-# @raycast.needsConfirmation false
-#
-# Documentation:
-# @raycast.description Open recent Xcode project
+# @raycast.title Xcode
 # @raycast.author Sergey Korobyin
 # @raycast.authorURL https://github.com/huangsemao
+# @raycast.description Open recent xcode project
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Recent Xcode project
+# @raycast.mode silent
+# @raycast.icon images/xcode.png
+# @raycast.packageName Xcode
+# @raycast.argument1 { "type": "text", "placeholder": "Project name", "percentEncoded": true}
 
 mdfind "(kMDItemFSName == '*$1*'c) && ((kMDItemContentType =='com.apple.xcode.project') || (kMDItemContentType == 'com.apple.dt.document.workspace') || (kMDItemContentType == 'com.apple.dt.playground'))" -0 | xargs -0 ls -t | head -1 | cut -f1 -d: | awk '{print "\x27" $0 "\x27" }' | xargs  open
