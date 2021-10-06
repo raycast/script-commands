7a634ef2dcfec2fd64ac42609ae4f9c7fe754310
Sergey Korobyin <sergey.korobyin@yellowcat.io>
2021-10-06 15:53:54 +0300

    Add commands to run Endel modes and scenarios

---
+++ commands/media/endel/endel-scenarios-unwind.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Limitation: Endel URL scheme is not final and could be changed in the future
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Unwind
+# @raycast.mode silent
+# @raycast.packageName Endel
+# @raycast.argument1 { "type": "text", "placeholder": "Minutes", "percentEncoded": false}
+#
+# Optional parameters:
+# @raycast.icon images/endel.png
+# @raycast.needsConfirmation false
+#
+# Documentation:
+# @raycast.description Run Unwind scenario at Endel 
+# @raycast.author Sergey Korobyin
+# @raycast.authorURL https://github.com/huangsemao
+
+MINUTES=$1
+MINUTES=$((MINUTES * 60))
+open "endel://?type=scenario&value=unwind&duration=$MINUTES"
