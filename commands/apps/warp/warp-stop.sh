1524aff921f27ea05792d614d40f1cca9b7c5b83
Sergey Fuksman <sergey@fuksman.com>
2021-11-03 23:01:39 +0300

    Add warp: check and toggle Cloudflare WARP connection (#607)
    * Add warp: check and toggle Cloudflare WARP connection
    * Changed command names due to @unnamedd comment
    
    Co-authored-by: Thiago Holanda <unnamedd@gmail.com>

---
+++ commands/apps/warp/warp-stop.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Dependency: Cloudflare WARP https://developers.cloudflare.com/warp-client/setting-up/macOS
+# Note: Cloudflare WARP must be installed, but CLI works properly only if GUI-client is not running
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Disconnect
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon images/warp.png
+
+# @Documentation:
+# @raycast.packageName WARP
+# @raycast.description Disconnect from WARP
+# @raycast.author Sergey Fuksman
+# @raycast.authorURL https://github.com/fuksman
+
+
+if ! command -v warp-cli &> /dev/null; then
+  echo "WARP is required (https://developers.cloudflare.com/warp-client/setting-up/macOS)";
+  exit 1;
+fi
+
+# Source: https://superuser.com/a/736859
+function isnt_connected () {
+    warp-cli status | sed -n 2p | grep -qv Connected
+}
+
+
+if isnt_connected; then
+    echo "WARP is not connected"
+    exit 1
+else
+    warp-cli disconnect
+    echo "Disconnected from WARP"
+fi
