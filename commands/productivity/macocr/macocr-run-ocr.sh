9639ce73e634eaf60e8d8ebbe3c7c2d6d6869df5
Jakub Lanski <jakub.lanski@roche.com>
2021-11-10 12:33:47 +0100

    Add language option support

--- commands/productivity/macocr/macocr-run-ocr.sh
+++ commands/productivity/macocr/macocr-run-ocr.sh
@@ -14,10 +14,11 @@
 # Optional parameters:
 # @raycast.packageName macOCR
 # @raycast.icon 📸
+# @raycast.argument1 { "type": "text", "placeholder": "Language (default: en-US)", "optional": true }
 
 # Documentation:
 # @raycast.author Jakub Lanski
 # @raycast.authorURL https://github.com/jaklan
 
-result=$(ocr)
+result=$(ocr -l ${1:-"en-US"})
 echo $result | tee >(pbcopy)
