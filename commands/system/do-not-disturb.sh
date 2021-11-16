07b47838a4d0ac62c0d73afb60dfa86fb96ea36d
Jakub Lański <jakub.lanski@roche.com>
2021-11-16 14:59:56 +0100

    Update Do Not Disturb command (#624)
    * Update Do Not Disturb command
    * Update shebang

--- commands/system/do-not-disturb.sh
+++ commands/system/do-not-disturb.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Dependency: This script requires `calm-notifications` cli installed: https://github.com/vitorgalvao/tiny-scripts/blob/master/calm-notifications
 # Install via homebrew: `brew install vitorgalvao/tiny-scripts/calm-notifications`
@@ -11,18 +11,19 @@
 # Optional parameters:
 # @raycast.icon 🔕
 # @raycast.packageName System
-# @raycast.argument1 { "type": "text", "placeholder": "command", "optional": true, "percentEncoded": true }
+# @raycast.argument1 { "type": "text", "placeholder": "Command (default: toggle)", "optional": true, "percentEncoded": true }
 #
 # @Documentation:
 # @raycast.description Do Not Disturb
 # @raycast.author Antonio Dal Sie
 # @raycast.authorURL https://github.com/exodusanto
 
-if [[ "$1" != "on" && "$1" != "off" && "$1" != "toggle" ]]; then
-  echo "Unsupported parameter value: $1 (must be [on/off/toggle])"
+cmd=${1:-"toggle"}
+
+if [[ "$cmd" != "on" && "$cmd" != "off" && "$cmd" != "toggle" ]]; then
+  echo "Unsupported command: $1 (must be [on/off/toggle])"
   exit 1
 fi
 
-calm-notifications $1
-sleep 2
+calm-notifications $cmd
 echo "Do Not Disturb $(calm-notifications status)"
