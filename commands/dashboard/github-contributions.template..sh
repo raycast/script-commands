0e807e1fa41b933f09ee644546f582cb61df4053
astrit <am@astrit.co>
2021-11-19 02:21:48 +0100

    Add GitHub Contributions Script

---
+++ commands/dashboard/github-contributions.template..sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title GitHub Contributions
+# @raycast.mode inline
+# @raycast.refreshTime 12h
+
+# Optional parameters:
+# @raycast.packageName GitHub Contributions
+# @raycast.icon images/github.png
+
+# Documentation:
+# @raycast.description Show GitHub user contributions from the current year
+# @raycast.author Astrit
+# @raycast.authorURL https://github.com/astrit
+
+# GitHub username is required
+username=""
+
+url="https://github.com/users/${username}/contributions"
+result=$(curl -s $url )
+resultFilter=$(echo "$result" | sed -n -e '/<h2 class="f4 text-normal mb-2">/,/<\/h2>/p')
+resultCleanup=$(echo "$resultFilter" | sed -e 's/<[^>]*>//g')
+
+echo $resultCleanup
