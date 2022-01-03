6a9228d64f776d138acbce02b8122514540d276e
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-01-03 14:02:54 +0100

    Add microlink command (#643)
    * Add microlink command
    * Update microlink.sh
    * use a static image

---
+++ commands/developer-utils/microlink.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Dependency: This script requires `microlink` CLI installed: https://microlink.io/docs/api/getting-started/cli
+# Install via npm: `npm install -g microlink`
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Microlink API
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon images/microlink.png
+# @raycast.argument1 { "type": "text", "placeholder": "url", "optional": false }
+
+# Documentation:
+# @raycast.description Microlink API integration
+# @raycast.author Kiko Beats
+
+microlink "$1"
