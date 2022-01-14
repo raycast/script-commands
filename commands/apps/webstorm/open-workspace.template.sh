e4de4ec7ea8d8ce7fdfe2024d09a8cee640c96fe
Danila, Stavrov <dan.stavrov@gmail.com>
2022-01-14 04:25:25 +0300

    Change filename and comments after read contributing guideline

--- commands/apps/webstorm/open-workspace.template.sh
+++ commands/apps/webstorm/open-workspace.template.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# Note: WebStorm required
+# Install via via JetBrains Toolbox https://www.jetbrains.com/toolbox-app/
+
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title Open WebStorm
 # @raycast.mode compact
-# @raycast.packageName WebStorm
 #
 # Optional parameters:
 # @raycast.icon 👨‍💻
+# @raycast.packageName WebStorm
+#
+# Arguments
 # @raycast.argument1 { "type": "text", "placeholder": "Workspace", "optional": true }
 # @raycast.argument2 { "type": "text", "placeholder": "Close others? [yes/no]", "optional": true }
 #
@@ -16,18 +21,22 @@
 # @raycast.author Daniel Stovv
 # @raycast.authorURL https://github.com/stovv
 #
-# Requirements
-# WebStorm installed via JetBrains Toolbox
-
-# arguments
-WORKSPACE=$1
-CLOSE_OTHER=$2
-
-# Configuration:
+# Configuration
 # 1. Please make sure for webstorm installed and webstorm script available in PATH
 #    https://www.jetbrains.com/help/webstorm/working-with-the-ide-features-from-command-line.html#standalone
 # 2. Configure always open projects in new window Preferences -> Appearance & Behavior -> System Settings -> Project -> Open project in [ Select: New Window ]
 # 3. Add more workspaces by example
+
+if ! command -v webstorm &> /dev/null; then
+      echo "WebStorm CLI is required (https://www.jetbrains.com/help/webstorm/working-with-the-ide-features-from-command-line.html#standalone).";
+      exit 1;
+fi
+
+# Main program
+
+# Arguments
+WORKSPACE=$1
+CLOSE_OTHER=$2
 
 PROCESS=$(pgrep -f webstorm)
 if [ "${CLOSE_OTHER}" = "yes" ] && [ "${PROCESS}" != "" ] ;then
