0371bde79b3c0817317ca5197acdc18e3c5b8a62
Danila, Stavrov <dan.stavrov@gmail.com>
2022-01-27 14:17:54 +0300

    refactor: Apply suggestions

--- commands/apps/webstorm/open-workspace.template.sh
+++ commands/apps/webstorm/open-workspace.template.sh
@@ -5,24 +5,24 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Open WebStorm
+# @raycast.title Open
 # @raycast.mode compact
 #
 # Optional parameters:
-# @raycast.icon 👨‍💻
+# @raycast.icon images/webstorm.png
 # @raycast.packageName WebStorm
 #
 # Arguments
 # @raycast.argument1 { "type": "text", "placeholder": "Workspace", "optional": true }
-# @raycast.argument2 { "type": "text", "placeholder": "Close others? [yes/no]", "optional": true }
+# @raycast.argument2 { "type": "text", "placeholder": "Close Others? [yes/no]", "optional": true }
 #
 # Documentation:
-# @raycast.description Open webstorm projects
+# @raycast.description Open WebStorm projects
 # @raycast.author Daniel Stovv
 # @raycast.authorURL https://github.com/stovv
 #
 # Configuration
-# 1. Please make sure for webstorm installed and webstorm script available in PATH
+# 1. Please make sure for WebStorm installed and WebStorm CLI available in $PATH
 #    https://www.jetbrains.com/help/webstorm/working-with-the-ide-features-from-command-line.html#standalone
 # 2. Configure always open projects in new window Preferences -> Appearance & Behavior -> System Settings -> Project -> Open project in [ Select: New Window ]
 # 3. Add more workspaces by example
@@ -40,21 +40,17 @@ CLOSE_OTHER=$2
 
 PROCESS=$(pgrep -f webstorm)
 if [ "${CLOSE_OTHER}" = "yes" ] && [ "${PROCESS}" != "" ] ;then
-  echo "Kill webstorm..."
+  echo "Kill WebStorm..."
   kill -9 "${PROCESS}"
 fi
 
 # Example:
 if [[ "${WORKSPACE}" = "workspace_name" ]];then
-  echo "Open ${WORKSPACE} workspace"
-
   # TODO: change paths to projects
   webstorm nosplash ~/Project/someProject ~/Project/someOtherProject
   echo "Workspace ${WORKSPACE} opened!"
   exit 0
 fi
 
-echo "Open webstorm projects dialog..."
 webstorm nosplash dontReopenProjects
-echo "Open webstorm Done!"
-
+echo "WebStorm projects dialog opened!"
