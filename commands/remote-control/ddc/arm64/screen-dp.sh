2f5e6c59e3426222c3f0cbf0376464796c471d5b
goodhyun <goodhyun@gmail.com>
2022-02-22 16:29:39 +0900

    Create screen-dp.sh

---
+++ commands/remote-control/ddc/arm64/screen-dp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Screen DP
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 📺
+# @raycast.author goodhyun
+
+if ! command -v m1ddc &> /dev/null; then
+      echo "m1ddc command is required (https://github.com/waydabber/m1ddc).";
+      exit 1;
+fi
+
+m1ddc set input 15
