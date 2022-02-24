5c6dc3b24bbda620f63426615209cbfa1ce5e32b
goodhyun <goodhyun@gmail.com>
2022-02-24 11:54:19 +0900

    Update screen-hdmi.sh

--- commands/remote-control/ddc/x86/screen-hdmi.sh
+++ commands/remote-control/ddc/x86/screen-hdmi.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Dependency: This script requires `ddcctl` cli installed: https://github.com/kfix/ddcctl
+# Install via homebrew: `brew install ddcctl`
+
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Screen HDMI
+# @raycast.title Switch to HDMI
 # @raycast.mode compact
 
 # Optional parameters:
 # @raycast.icon 📺
 # @raycast.author goodhyun
+# @raycast.packageName External Display
+# @raycast.description This script will switch the external display screen to HDMI.
+
 
 if ! command -v ddcctl &> /dev/null; then
       echo "ddcctl command is required (https://github.com/kfix/ddcctl).";
