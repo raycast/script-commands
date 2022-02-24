ef486970d61f36f746428a173cb783f7625a49ee
goodhyun <goodhyun@gmail.com>
2022-02-24 13:02:35 +0900

    Update screen-hdmi.sh

--- commands/remote-control/ddc/arm64/screen-hdmi.sh
+++ commands/remote-control/ddc/arm64/screen-hdmi.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Dependency: This script requires `m1ddc` cli installed: https://github.com/waydabber/m1ddc
+# Install via github: `https://github.com/waydabber/m1ddc`
+
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Screen HDMI
+# @raycast.title Switch to HDMI
 # @raycast.mode compact
 
 # Optional parameters:
 # @raycast.icon 📺
 # @raycast.author goodhyun
+# @raycast.packageName External Display
+# @raycast.description This script will switch the external display screen to HDMI.
+
 
 if ! command -v m1ddc &> /dev/null; then
       echo "m1ddc command is required (https://github.com/waydabber/m1ddc).";
