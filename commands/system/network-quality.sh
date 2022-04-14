3f6718fbd88bfb6ddf5f83dea7e2db394a4644e5
LanikSJ <laniksj@gmail.com>
2022-04-14 09:55:39 -0700

    Update Script from PR Comments

--- commands/system/network-quality.sh
+++ commands/system/network-quality.sh
@@ -12,4 +12,10 @@
 # @raycast.author Archie Lacoin & LanikSJ
 # @raycast.authorURL https://github.com/pomdtr & https://github.com/LanikSJ
 
-networkQuality -v
+result=$(networkQuality -v)
+
+rtt=$(echo "$result" | grep RTT | awk -F": " '{print $2}')
+mbps_down=$(echo "$result" | grep "Download capacity" | awk -F": " '{print $2}')
+mbps_up=$(echo "$result" | grep "Upload capacity" | awk -F": " '{print $2}')
+
+echo "↓ ${mbps_down}  ↑ ${mbps_up}  ↔ ${rtt} ms"
