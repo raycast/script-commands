f33ae06398071039ff57b624335faa9383e7ee5f
LanikSJ <laniksj@gmail.com>
2022-04-13 20:19:15 -0700

    Add Network Quality CLI

---
+++ commands/system/network-quality.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Network Quality
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🌐
+
+# Documentation:
+# @raycast.author LanikSJ
+# @raycast.authorURL https://github.com/LanikSJ
+
+networkQuality -v
