51d7ff0993807cff3d04e0be81d38b39d970a133
Andrew Truex <andrewtruex@gmail.com>
2022-05-14 03:39:19 -0400

    Add Port 3000
    Opens localhost at port 3000 in browser

---
+++ commands/navigation/port-3000.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Port 3000
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🌐
+
+# Documentation:
+# @raycast.description Opens localhost at port 3000
+# @raycast.author Andrew Truex
+# @raycast.authorURL https://github.com/ajtruex
+
+open http://localhost:3000
