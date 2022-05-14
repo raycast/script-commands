5d4efc95e57871ec8c15e4851c4df61a945a51df
Andrew Truex <andrewtruex@gmail.com>
2022-05-14 03:46:46 -0400

    Add Port 8080

---
+++ commands/navigation/port-8080.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Port 8080
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🌐
+
+# Documentation:
+# @raycast.description Opens localhost at port 8080
+# @raycast.author Andrew Truex
+# @raycast.authorURL https://github.com/ajtruex
+
+open http://localhost:8080
+
