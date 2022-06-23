a367f984c50186b598136e6ad0fd92bd580f285e
Fatpandac <i@Fatpandac.com>
2022-06-23 18:43:46 +0800

    fix: add list to support other language messages

--- commands/apps/Message/copycode.sh
+++ commands/apps/Message/copycode.sh
@@ -2,14 +2,15 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title CopyCode
+# @raycast.title Copy Code
 # @raycast.mode silent
+# @raycast.packageName Messages
 
 # Optional parameters:
 # @raycast.icon images/Message.png
 
 # Documentation:
-# @raycast.description Copy code from chinese message.
+# @raycast.description Copy verification code from a message.
 # @raycast.author Fatpandac
 # @raycast.authorURL https://github.com/Fatpandac
 
@@ -17,17 +18,19 @@ USERNAME=$(id -u -n)
 
 result=$(sqlite3 /Users/$USERNAME/Library/Messages/chat.db 'SELECT text FROM message WHERE datetime(date/1000000000 + 978307200,"unixepoch","localtime") > datetime("now","localtime","-60 second") ORDER BY date DESC LIMIT 1;')
 
-keyword="验证码";
+#You can append another `keyword` into the list to support other language Messages.
+keyword=("验证码" "code");
+keyword_regex="^(.*)(${keyword[*]/%/|})(.*)$"
 
 if [ ! $result ]; then
-  echo "最近60秒未收到验证码！"
+  echo "No verification code received in the last 60 seconds!"
   exit 0;
 fi
 
-if [[ "$result" =~ "$keyword" ]]; then
+if [[ "$result" =~ "$keyword_regex" ]]; then
   code=`echo $result | grep -o "[0-9]\{4,6\}"`;
 
   echo "$code" | pbcopy;
-  echo "$code 验证码已复制"
+  echo "$code code copied!"
 fi
 
