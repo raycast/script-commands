753f9a8d3b0ba09e06251b16e28d2bb51ae906fc
Lukas Oppermann <oppermann.lukas@gmail.com>
2022-07-01 16:07:45 +0200

    Updated current-weather to use better geo location (#741)
    * Updated current-weather to use better geo location
    
    * Update current-weather.sh

--- commands/dashboard/current-weather.sh
+++ commands/dashboard/current-weather.sh
@@ -15,9 +15,18 @@
 ## (see https://github.com/chubin/wttr.in/blob/master/share/static/malformed-response.html).
 # @raycast.refreshTime 1h
 
-output=$(curl -s https://wttr.in/?format="%c+%f+%w{;}%l")
+# get your ip
+IP=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | sed -r 's/^"|"$//g')
 
-IFS='{;}' read -ra array <<< "$output"
-IFS=',' read -ra location <<< "${array[1]}"
+# configure
+UNIT="m" # celsius
+# UNIT="u" # fahrenheit
 
-echo "${array[0]} - ${location[0]}"
+# Automatic location
+LOCATION=$(curl --silent "https://ipapi.co/${IP}/postal/") # automatic location
+CITY=$(curl --silent "https://ipapi.co/${IP}/city/") # for output
+# Manual location
+# LOCATION="Berlin" # use this if you want to manually set your location
+# CITY="${LOCATION}" 
+
+curl --silent "https://wttr.in/${LOCATION}?format=${CITY}:+%c+%f+%w&${UNIT}"
