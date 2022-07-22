f436f93f90043aaae10d3df313146b7b243ef340
razvanazamfirei <azamfireirazvan@gmail.com>
2022-07-22 06:29:42 -0400

    Fix bugs and changed file name (#744)
    * Fix bugs and changed file name
    
    Updated API call to new endpoint, changed add method to "quick add" to reduce the number of arguments, cleaned up redundant code
    
    * Update commands/apps/todoist/create-task.sh
    
    Co-authored-by: Thiago Holanda <unnamedd@gmail.com>
    
    * Update commands/apps/todoist/create-task.sh
    
    Co-authored-by: Thiago Holanda <unnamedd@gmail.com>
    
    * Update commands/apps/todoist/create-task.sh
    
    Co-authored-by: Thiago Holanda <unnamedd@gmail.com>
    
    * Rename create-task.sh to create-task.template.sh
    
    Co-authored-by: Thiago Holanda <unnamedd@gmail.com>

--- commands/apps/todoist/create-task.template.sh
+++ commands/apps/todoist/create-task.template.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 # Required parameters:
-# @raycast.author Faris Aziz
-# @raycast.authorURL https://github.com/farisaziz12
 # @raycast.schemaVersion 1
 # @raycast.title Create Task
 # @raycast.mode silent
-# @raycast.packageName Todoist
-# @raycast.description Creates Todoist task
-# @raycast.needsConfirmation false
-# @raycast.argument1 { "type": "text", "placeholder": "Buy Milk", "optional": false, }
-# @raycast.argument2 { "type": "text", "placeholder": "Due (Defaults to Tomorrow)", "optional": true, }
-
 
 # Optional parameters:
 # @raycast.icon images/todoist-logo.png
+# @raycast.packageName Todoist
+# @raycast.argument1 { "type": "text", "placeholder": "Call Mom Tomorrow at 5" }
+
+# Documentation: 
+# @raycast.description Create Task
+# @raycast.author Faris Aziz
+# @raycast.authorURL https://github.com/farisaziz12
 
 # Get your API Token from: https://todoist.com/prefs/integrations
 
-API_TOKEN=
+API_TOKEN="APITOKENHERE"
 
 if [ -z "$API_TOKEN" ]; then
 	echo "Todoist API token is missing.";
@@ -26,16 +25,11 @@ if [ -z "$API_TOKEN" ]; then
 fi
 
 TASK="$1"
-DUE="Tomorrow"
-
-if [[ $2 != "" ]]; then
-    DUE=$2
-fi
 
 if [[ $TASK != "" ]]; then
-    curl -s "https://api.todoist.com/rest/v1/tasks" \
-    	-X POST \
-    	--data '{"content": "'"$TASK"'", "due_string": "'"$DUE"'"}' \
+    curl "https://api.todoist.com/sync/v8/quick/add" \
+    	-X -S POST \
+    	--data '{"text": "'"$TASK"'"}' \
     	-H "Content-Type: application/json" \
     	-H "Authorization: Bearer $API_TOKEN"
 
