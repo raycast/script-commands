e5c52ba0bbc92fff9e8dff62b6a162ec8f620cd9
JD Solanki <47495003+jd-solanki@users.noreply.github.com>
2022-10-31 16:47:30 +0530

    feat(browsing): Open in guest profile
    Open current website in guest profie/mode

---
+++ commands/browsing/open-in-guest-profile.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open in guest profile
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Browser
+
+# Documentation:
+# @raycast.description Open current website in guest profie/mode
+# @raycast.author JD Solanki
+# @raycast.authorURL https://github.com/jd-solanki
+
+############################################
+## Set your browser app                   ##
+## "Google Chrome"                        ##
+## "Brave Browser"                        ##
+############################################
+
+browser="Google Chrome"
+
+URL=$(osascript -e "tell application \"$browser\" to get URL of active tab of first window")
+
+open -a "$browser" -n --args --guest --new-window "$URL"
+
