6412b8f069dd07aae9ed561a4879062759859049
Razvan Azamfirei <azamfireirazvan@gmail.com>
2022-12-16 16:54:47 -0500

    feat(browsing): command for doi string parsing
    This is an additional command based on my previous
    commit that scans the clipboard for doi strings
    (rather than waiting for the user to paste the
    string) and opens the corresponding resource.
    
    Signed-off-by: Razvan Azamfirei <azamfireirazvan@gmail.com>
    
    feat(browsing): command for doi string parsing
    
    This is a package that parses a doi string (unique digital object identifier used to identify academic, professional, and government information) and opens the corresponding resource.

---
+++ commands/web-searches/doi-clipboard.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Find Paper
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📖
+# @raycast.packageName DOI
+
+# Documentation:
+# @raycast.description Scans clipboard and opens DOI links in your browser
+# @raycast.author Razvan Azamfirei
+# @raycast.authorURL https://github.com/razvanazamfirei
+LINK="$(pbpaste)"
+REGEX="^doi: 10."
+if [[ "$LINK" =~ ^doi:10. ]]; then
+    IN="$LINK"
+    arrIN=("${IN//doi:/}")
+    URL="https://doi.org/${arrIN[0]}"
+    open "$URL"
+elif [[ "$LINK" =~ ^doi/10. ]]; then
+    IN="$LINK"
+    arrIN=("${IN//doi///}")
+    URL="https://doi.org/${arrIN[0]}"
+    open "$URL"
+elif [[ "$LINK" =~ ${REGEX} ]]; then
+    IN="$LINK"
+    arrIN=("${IN//doi: //}")
+    URL="https://doi.org/${arrIN[0]}"
+    open "$URL"
+elif [[ "$LINK" =~ ^10. ]]; then
+    URL="https://doi.org/${LINK}"
+    open "$URL"
+else
+    echo "Please specify a DOI"
+    exit 1
+fi
