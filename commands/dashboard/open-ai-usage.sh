8dd5b549df51f4b7ac842440df8fe44e7b6728d0
mie035 <Notakes035@gmail.com>
2023-05-04 00:27:11 +0900

    add check open api usage

---
+++ commands/dashboard/open-ai-usage.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Check Usage of OpenAI API
+# @raycast.mode inline
+# @raycast.packageName System
+
+#
+# Optional parameters:
+# @raycast.icon 💲
+#
+# Documentation:
+# @raycast.description Get total usage of OpenAI API
+# @raycast.author nagauta
+# @raycast.authorURL https://github.com/nagauta
+
+_ORGANIZATION=""
+_OPENAI_APIKEY=""
+
+_START_DATE="$(date -u +'%Y-%m')-01"
+_END_DATE="$(date -u -v+1m +'%Y-%m')-01"
+echo 
+TOTAL_USAGE=$(curl -sSf "https://api.openai.com/dashboard/billing/usage?end_date=${_END_DATE}&start_date=${_START_DATE}" \
+-H "authorization: Bearer ${_OPENAI_APIKEY}" \
+| jq -r '"$" + (.total_usage | round / 100 | tostring)')
+
+echo "Total Usage: ${TOTAL_USAGE}, Period: ${_START_DATE}-${_END_DATE}"
