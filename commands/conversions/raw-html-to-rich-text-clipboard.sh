9b93656f30e11029b689b11708f1b9b82bb237c1
Michael Bianco <iloveitaly@gmail.com>
2023-05-16 08:56:49 -0600

    feat: convert raw html on clipboard to rich text

---
+++ commands/conversions/raw-html-to-rich-text-clipboard.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Convert Raw HTML to Rich Text on Clipboard
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📋
+# @raycast.packageName Conversions
+# @raycast.description This script will convert raw HTML on your clipboard to rich text. It requires pandoc to be installed on your system.
+
+# Documentation:
+# @raycast.author Michael Bianco
+# @raycast.authorURL https://github.com/iloveitaly
+
+pbpaste | pandoc -f html -t rtf -s | pbcopy -pboard general -Prefer rtf
