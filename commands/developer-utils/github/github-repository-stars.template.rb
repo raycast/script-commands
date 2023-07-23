2ee581e812da724864394c2490a6a85cb51a51b1
Valdir Junior <59587473+valdirjunior011@users.noreply.github.com>
2023-07-23 14:36:01 +0200

    Update github-repository-stars.template.rb
    Return the stats of all repos by the owner of the repository.

--- commands/developer-utils/github/github-repository-stars.template.rb
+++ commands/developer-utils/github/github-repository-stars.template.rb
@@ -1,10 +1,8 @@
 #!/usr/bin/env ruby
 
 # How to use this script?
-# It's a template which needs further setup. Duplicate the file, 
-# remove `.template.` from the filename, set an API token and 
-# specify your repository.
-
+# It's a template which needs further setup. Duplicate the file,
+# remove `.template.` from the filename, set an API token.
 
 # Parameters
 
@@ -19,19 +17,17 @@
 # @raycast.icon ⭐️
 
 # Documentation:
-# @raycast.description Show statistics of your GitHub repository.
+# @raycast.description Show statistics of your GitHub repositories.
 # @raycast.author Thomas Paul Mann
 # @raycast.authorURL https://github.com/thomaspaulmann
-
 
 # Configuration
 
 # Insert a token with access to your repository (https://github.com/settings/tokens)
 $api_token = ""
 
-# Slug of the owner and repository
-$repository = "raycast/script-commands"
-
+# Owner of the repositories
+$owner = ""
 
 # Main program
 
@@ -39,12 +35,12 @@ require "json"
 require "net/http"
 require "uri"
 
-if $api_token.empty?
-  puts "No API token provided"
+if $api_token.nil?
+  puts "API token not provided"
   exit(1)
 end
 
-uri = URI("https://api.github.com/repos/#{$repository}")
+uri = URI("https://api.github.com/users/#{$owner}/repos")
 req = Net::HTTP::Get.new(uri)
 req["Authorization"] = "token #{$api_token}"
 
@@ -57,14 +53,41 @@ res = Net::HTTP.start(uri.hostname, uri.port, req_options) { |http|
 }
 
 if res.code == "200"
-  result = JSON.parse(res.body)
+  repositories = JSON.parse(res.body)
 
-  stars = result["stargazers_count"] 
-  forks = result["forks_count"] 
-  subscribers = result["subscribers_count"] 
-  
-  puts "#{stars} Stars, #{forks} Forks and #{subscribers} Watchers"
-else 
-  puts "Failed loading GitHub repository"
-  exit(1)
+  total_stars = 0
+  total_forks = 0
+  total_subscribers = 0
+
+  repositories.each do |repo|
+    repo_name = repo["full_name"]
+    url = repo["html_url"]
+
+    repo_uri = URI("https://api.github.com/repos/#{repo_name}")
+    repo_req = Net::HTTP::Get.new(repo_uri)
+    repo_req["Authorization"] = "token #{$api_token}"
+
+    repo_res = Net::HTTP.start(repo_uri.hostname, repo_uri.port, req_options) { |http|
+      http.request(repo_req)
+    }
+
+    if repo_res.code == "200"
+      result = JSON.parse(repo_res.body)
+
+      stars = result["stargazers_count"]
+      forks = result["forks_count"]
+      subscribers = result["subscribers_count"]
+
+      total_stars += stars
+      total_forks += forks
+      total_subscribers += subscribers
+
+    else
+      puts "Failed loading statistics for #{repo_name}"
+    end
+  end
+
+  puts "Owner: #{$owner} Total Stars: #{total_stars}, Forks: #{total_forks}, Subs: #{total_subscribers}"
+else
+  puts "Failed loading repositories for #{$owner}"
 end
