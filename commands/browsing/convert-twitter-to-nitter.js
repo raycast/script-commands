35a0cdc59d1c050640f3d665758b885bfda8643f
noidwasavailable <26755327+noidwasavailable@users.noreply.github.com>
2023-08-04 15:41:09 +0900

    ✨ add command to convert twitter link to nitter

---
+++ commands/browsing/convert-twitter-to-nitter.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+// Required parameters:
+// @raycast.schemaVersion 1
+// @raycast.title Convert Twitter to Nitter
+// @raycast.mode silent
+
+// Optional parameters:
+// @raycast.icon 🐔
+
+// Documentation:
+// @raycast.description Convert Twitter link to Nitter
+// @raycast.author csharp
+// @raycast.authorURL https://raycast.com/csharp
+// @raycast.argument1 { "type": "text", "placeholder": "Twitter Link" }
+
+// Example Twitter link: https://twitter.com/Cron/status/1644010827647975425
+const child_process = require('child_process');
+let [link] = process.argv.slice(2);
+
+if (!link) {
+  console.error('No link provided');
+  return;
+}
+
+try {
+  link = link.replace('twitter.com', 'nitter.net');
+  console.log(`Opening ${link}`);
+  child_process.execSync(`open "${link}"`);
+  return;
+} catch (error) {
+  console.error('Invalid link provided');
+  return;
+}
