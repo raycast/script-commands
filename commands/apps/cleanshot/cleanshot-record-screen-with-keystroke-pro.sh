f1250c675c9b7f30108843ac6c768fcc8a5a7ef6
Danylo Zalizchuk <danilmail0110@gmail.com>
2023-08-12 12:45:20 +0300

    Add Record Screen With Keystroke Pro command

---
+++ commands/apps/cleanshot/cleanshot-record-screen-with-keystroke-pro.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Note: CleanShot X required
+# Install from https://cleanshot.com
+#
+# Keystroke Pro required
+# Install from https://apps.apple.com/app/id1572206224?ct=ixeau
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Record Screen With Keystroke Pro
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ./images/record-screen-keystroke-pro.png
+# @raycast.packageName CleanShot X
+
+# Documentation:
+# @raycast.author danulqua
+# @raycast.authorURL https://raycast.com/danulqua
+# @raycast.description Launch Keystroke Pro and CleanShot X screen recording.
+# @raycast.description Start a screen recording with keystrokes using the Keystroke Pro app and save it as a video or an optimized GIF file.
+
+open "/Applications/Keystroke Pro.app"
+open "cleanshot://record-screen"
