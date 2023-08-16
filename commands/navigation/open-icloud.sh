cff6a5171f89c04ec63a35e2861cd78ea82dbdd5
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2023-08-16 15:34:00 +0200

    feat: add open icloud

---
+++ commands/navigation/open-icloud.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open iCloud
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon images/icloud.png
+# @raycast.packageName Navigation
+
+# Documentation:
+# @raycast.description Opens the iCloud folder in the Finder.
+# @raycast.author Danny Seidel
+# @raycast.authorURL https://raycast.com/danny_seidel
+
+open ~/Library/Mobile\ Documents/com~apple~CloudDocs
