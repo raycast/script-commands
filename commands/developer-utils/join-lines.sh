a22a3998fce638c9dfc85335ed61a0c7fa350972
decaylala <decaylala@users.noreply.github.com>
2023-08-28 14:36:31 +0800

    update the authorURL metatag
    Co-authored-by: Thiago Holanda <unnamedd@gmail.com>

--- commands/developer-utils/join-lines.sh
+++ commands/developer-utils/join-lines.sh
@@ -13,6 +13,7 @@
 # Documentation:
 # @raycast.description Join multiple lines of text from the clipboard into a single line, separated by a specified delimiter.
 # @raycast.author decaylala
+# @raycast.authorURL https://github.com/decaylala
 
 pbpaste | awk -v d="$1" 'BEGIN {ORS=d} {print}' | awk -v d="$1" 'sub(d "$", "")' | pbcopy
 echo 'Copied to clipboard'
