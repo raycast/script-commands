212044be2fe505fbf9921a3c79b2f7abc86b7dcb
Atsushi Kawamura (atzz/a2c) <atsushi1230.sdi@gmail.com>
2023-10-17 22:58:55 +0900

    Improvement: Add Description and Set Default Title with Optional Argument (#884)
    * feat: add description
    
    (cherry picked from commit 61c79645d6050e02a2b12bd5c1d19b61df0a12d5)
    
    * feat: set default title and update title argument optional
    
    (cherry picked from commit 2fc559b4b03d6463e08ae0d48848c3df93222a9c)

--- commands/conversions/inputs-to-markdown-link.sh
+++ commands/conversions/inputs-to-markdown-link.sh
@@ -3,28 +3,32 @@
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title Simple Markdown Link Generator
+# @raycast.description Quickly and simply generate a markdown formatted link with your specified URL and title.
 # @raycast.mode silent
 # @raycast.packageName Conversions
 
 # Optional parameters:
 # @raycast.icon 🔗
-# @raycast.argument1 { "type": "text", "placeholder": "Title" }
-# @raycast.argument2 { "type": "text", "placeholder": "URL" }
+# @raycast.argument1 { "type": "text", "placeholder": "URL" }
+# @raycast.argument2 { "type": "text", "placeholder": "Title (default: Link)", "optional": true }
 
 # Description:
-# Quickly and simply generate a markdown formatted link with your specified title and URL.
+# Quickly and simply generate a markdown formatted link with your specified URL and title.
 # The generated link is copied to your clipboard for easy pasting.
 
 # Documentation:
 # @raycast.author atzzCokeK
 # @raycast.authorURL https://github.com/atzzCokeK
 
+URL=$1
+TITLE=${2:-Link}  # Use "Link" as default if title is not provided
+
 regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]+'
 
-if [[ $2 =~ $regex ]]; then
-    echo "[$1]($2)" | pbcopy
-    echo "Copied: [$1]($2)"
+if [[ $URL =~ $regex ]]; then
+    echo "[$TITLE]($URL)" | pbcopy
+    echo "Copied: [$TITLE]($URL)"
 else
-    echo "Invalid URL: $2"
+    echo "Invalid URL: $URL"
     exit 1
 fi
