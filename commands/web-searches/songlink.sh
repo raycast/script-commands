a0f6ae48047d56069e013208662ddf953cc79d32
Alan Berman <796535+thealanberman@users.noreply.github.com>
2024-03-08 10:56:09 -0600

    Create songlink.sh
    https://song.link/ is a great way to share links to songs with others, since it offers links to most major streaming platforms. Your friend doesn't use Spotify? no problem.
    
    This does a search on song.link and grabs the top result to your clipboard.

---
+++ commands/web-searches/songlink.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Songlink
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🎵
+# @raycast.argument1 { "type": "text", "placeholder": "ARTIST + SONG OR ALBUM" }
+
+# Documentation:
+# @raycast.description Search + copy https://song.link URL to clipboard for easy sharing
+# @raycast.author Alan Berman
+# @raycast.authorURL https://github.com/thealanberman
+
+searchterm=$(IFS=" "; echo ${@})
+URL="https://itunes.apple.com/search?term=${searchterm}&country=US&entity=song,album"
+URL=${URL// /%20}
+# echo $URL
+track_id=$(curl --silent $URL | jq -r .results[0].trackId)
+echo "https://song.link/us/i/${track_id}" | pbcopy
