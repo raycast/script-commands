dea6e830de20dcbbafbc79503462cc3724549ecf
Maxim Borzov <max@borzov.com>
2024-05-07 09:23:36 +0300

    Update commands/conversions/markdown-to-telegram.py
    Co-authored-by: Thiago Holanda <unnamedd@gmail.com>

--- commands/conversions/markdown-to-telegram.py
+++ commands/conversions/markdown-to-telegram.py
@@ -16,6 +16,8 @@
 import re
 import subprocess
 
+env_vars = {'LANG': 'en_US.UTF-8'}
+encoding = 'utf-8'
 def paste():
     return subprocess.check_output('pbpaste', env={'LANG': 'en_US.UTF-8'}).decode('utf-8')
 
