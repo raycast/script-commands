d760ccef8bcbf15833eaeba78eb0d784c86fa10a
Sixeight <tomohiro68@gmail.com>
2025-01-23 18:29:53 +0900

    Add Random Emoji command

---
+++ commands/communication/emojis/random-emoji.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Dependency: This script requires `jq` cli installed: https://stedolan.github.io/jq/
+# Install via homebrew: `brew install jq`
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Random Emoji
+# @raycast.mode silent
+# @raycast.packageName Emojis
+
+# Optional parameters:
+# @raycast.icon 🎲
+
+# Documentation:
+# @raycast.description Copy a random emoji to the clipboard.
+# @raycast.author Tomohiro Nishimura
+# @raycast.authorURL https://github.com/Sixeight
+
+LANG="en_US.UTF-8"
+
+if ! command -v jq &> /dev/null; then
+      echo "jq command is required (https://github.com/jqlang/jq).";
+      exit 1;
+fi
+
+mapfile -t EMOJIS < <(curl -s https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json | jq -r '.[] | .emoji')
+EMOJI="${EMOJIS[$RANDOM % ${#EMOJIS[@]}]}"
+echo -n "$EMOJI" | pbcopy
+echo "$EMOJI"
