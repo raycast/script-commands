f836bca096e610eefee7d11b9252911903e21164
Vicent Gozalbes <vigosan@gmail.com>
2025-10-25 13:55:48 +0200

    Delete all files in single operation

--- commands/system/delete-selected-files.sh
+++ commands/system/delete-selected-files.sh
@@ -14,38 +14,22 @@
 # @raycast.author Vicent
 # @raycast.authorURL https://github.com/vigosan
 
-selected_files=$(osascript <<'ENDAPPLE'
+file_count=$(osascript <<'ENDAPPLE'
 tell application "Finder"
     set selectedItems to selection
     if (count of selectedItems) is 0 then
-        return "NO_SELECTION"
+        return 0
     end if
 
-    set filePaths to {}
-    repeat with anItem in selectedItems
-        set end of filePaths to POSIX path of (anItem as alias)
-    end repeat
-
-    set text item delimiters of AppleScript to linefeed
-    return filePaths as text
+    delete selectedItems
+    return count of selectedItems
 end tell
 ENDAPPLE
 )
 
-if [ "$selected_files" = "NO_SELECTION" ]; then
+if [ "$file_count" -eq 0 ]; then
     exit 1
 fi
-
-files=()
-while IFS= read -r line; do
-    [[ -n "$line" ]] && files+=("$line")
-done <<< "$selected_files"
-
-file_count=${#files[@]}
-
-for file in "${files[@]}"; do
-    osascript -e "tell application \"Finder\" to delete POSIX file \"$file\"" >/dev/null 2>&1
-done
 
 if [ $file_count -eq 1 ]; then
     message="1 item moved to Trash"
