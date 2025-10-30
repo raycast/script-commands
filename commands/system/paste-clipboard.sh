279aa339d3a2377c69ffda0118b0856125037548
AlexGadd <gadda023@gmail.com>
2025-10-30 19:04:17 +1300

    Added alert window and modifier key check
    Returns an alert window to tell the person not to hold modifier keys while the script runs, also added a brief pause before it runs to allow them to release the keys

--- commands/system/paste-clipboard.sh
+++ commands/system/paste-clipboard.sh
@@ -14,8 +14,30 @@
 # @raycast.author AlexGadd
 # @raycast.authorURL https://raycast.com/AlexGadd
 
+sleep 0.3
+#returns "true" or "false" if key is held down
+commandKeyDown=$(osascript -l JavaScript -e "ObjC.import('Cocoa'); ($.NSEvent.modifierFlags & $.NSEventModifierFlagCommand) > 1")
+controlKeyDown=$(osascript -l JavaScript -e "ObjC.import('Cocoa'); ($.NSEvent.modifierFlags & $.NSEventModifierFlagControl) > 1")
+optionKeyDown=$(osascript -l JavaScript -e "ObjC.import('Cocoa'); ($.NSEvent.modifierFlags & $.NSEventModifierFlagOption) > 1")
+shiftKeyDown=$(osascript -l JavaScript -e "ObjC.import('Cocoa'); ($.NSEvent.modifierFlags & $.NSEventModifierFlagShift) > 1")
+functionKeyDown=$(osascript -l JavaScript -e "ObjC.import('Cocoa'); ($.NSEvent.modifierFlags & $.NSEventModifierFlagFunction) > 1")
+
+keys=("$commandKeyDown" "$controlKeyDown" "$optionKeyDown" "$shiftKeyDown" "$functionKeyDown")
+
+anyPressed=false
+for key in "${keys[@]}"; do
+  if [[ $key == "true" ]]; then
+    anyPressed=true
+    break
+  fi
+done
+
+if $anyPressed; then
+osascript -e 'set theAlertText to "Modifier key held"' \
+-e 'set theAlertMessage to "To allow this script to function, please ensure you do not hold any modifier keys down while the paste script runs"' \
+-e 'display alert theAlertText message theAlertMessage as critical buttons {"OK"} default button "OK"' \
+else
 osascript -e 'set clipboardContent to the clipboard' \
--e 'delay 0.2' \
 -e 'set charCount to count of characters of clipboardContent' \
 -e 'tell application "System Events"' \
 -e '	repeat with i from 1 to charCount' \
@@ -23,3 +45,4 @@ osascript -e 'set clipboardContent to the clipboard' \
 -e '		keystroke theChar' \
 -e '	end repeat' \
 -e 'end tell'
+fi
