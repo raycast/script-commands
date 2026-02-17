78828dc68a1b7b0581d7894be22f9829570b1113
Morgan Williams <2504532+mrgnw@users.noreply.github.com>
2026-02-17 18:36:52 +0100

    Add Toggle Stage Manager script command

--- commands/system/toggle-stage-manager.sh
+++ commands/system/toggle-stage-manager.sh
@@ -3,7 +3,7 @@
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title Toggle Stage Manager
-# @raycast.mode compact
+# @raycast.mode silent
 # @raycast.packageName System
 
 # Optional parameters:
