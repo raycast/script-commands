15b56e667f91790fc5ac1a33de401c4a56999e5c
Morgan Williams <2504532+mrgnw@users.noreply.github.com>
2026-02-17 17:53:05 +0100

    Add Toggle Stage Manager script command

---
+++ commands/system/toggle-stage-manager.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Toggle Stage Manager
+# @raycast.mode compact
+# @raycast.packageName System
+
+# Optional parameters:
+# @raycast.icon 🪟
+
+# Documentation:
+# @raycast.description Toggle Stage Manager on or off
+# @raycast.author Morgan Williams
+# @raycast.authorURL https://www.raycast.com/morgan_williams
+
+STATE=$(defaults read com.apple.WindowManager GloballyEnabled 2>/dev/null)
+if [[ "${STATE}" = 0 ]]; then
+	defaults write com.apple.WindowManager GloballyEnabled -bool true
+	echo "Stage Manager enabled"
+else
+	defaults write com.apple.WindowManager GloballyEnabled -bool false
+	echo "Stage Manager disabled"
+fi
+killall WindowManager
