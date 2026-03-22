54f91b1811041170fc556989ad2694e541c6c21c
Muhammadrizo <muhammadrizo.dev@gmail.com>
2026-03-22 13:18:36 +0500

    Brightness control commands for Windows
    Increase brightness and decrease brightness commands for Windows

---
+++ commands/system/decrease-brightness-windows.ps1
@@ -0,0 +1,35 @@
+#!/usr/bin/env pwsh
+# @raycast.schemaVersion 1
+# @raycast.title Decrease Brightness
+# @raycast.mode compact
+
+# @raycast.icon ⬇️
+# @raycast.packageName Display
+# @raycast.description Decrease screen brightness by a given percentage.
+# @raycast.author muhammadrizo
+# @raycast.authorURL https://raycast.com/muhammadrizo
+
+# @raycast.argument1 { "type": "text", "placeholder": "Decrease by (%) – default 10", "optional": true }
+
+param(
+    [Parameter(Mandatory = $false, Position = 0)]
+    [string]$Delta
+)
+
+if ([string]::IsNullOrWhiteSpace($Delta)) {
+    $Delta = "10"
+}
+
+[int]$DeltaInt = -[math]::Abs([int]$Delta)
+
+$brightnessInfo = Get-WmiObject -Namespace root/wmi -Class WmiMonitorBrightness
+$current = $brightnessInfo.CurrentBrightness
+
+$new = $current + $DeltaInt
+if ($new -lt 0) { $new = 0 }
+if ($new -gt 100) { $new = 100 }
+
+$methods = Get-WmiObject -Namespace root/wmi -Class WmiMonitorBrightnessMethods
+$methods.WmiSetBrightness(1, [byte]$new)
+
+Write-Output "Set to: $new"
---
+++ commands/system/increase-brightness-windows.ps1
@@ -0,0 +1,35 @@
+#!/usr/bin/env pwsh
+# @raycast.schemaVersion 1
+# @raycast.title Increase Brightness
+# @raycast.mode compact
+
+# @raycast.icon ⬆️
+# @raycast.packageName Display
+# @raycast.description Increase screen brightness by a given percentage.
+# @raycast.author muhammadrizo
+# @raycast.authorURL https://raycast.com/muhammadrizo
+
+# @raycast.argument1 { "type": "text", "placeholder": "Increase by (%) – default 10", "optional": true }
+
+param(
+    [Parameter(Mandatory = $false, Position = 0)]
+    [string]$Delta
+)
+
+if ([string]::IsNullOrWhiteSpace($Delta)) {
+    $Delta = "10"
+}
+
+[int]$DeltaInt = $Delta
+
+$brightnessInfo = Get-WmiObject -Namespace root/wmi -Class WmiMonitorBrightness
+$current = $brightnessInfo.CurrentBrightness
+
+$new = $current + $DeltaInt
+if ($new -lt 0) { $new = 0 }
+if ($new -gt 100) { $new = 100 }
+
+$methods = Get-WmiObject -Namespace root/wmi -Class WmiMonitorBrightnessMethods
+$methods.WmiSetBrightness(1, [byte]$new)
+
+Write-Output "Set to: $new"
