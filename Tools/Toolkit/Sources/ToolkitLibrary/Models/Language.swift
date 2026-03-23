95f11d14d3260600c476991490e07d01e3ececba
Grzegorz Krukowski <grkrukowski@gmail.com>
2026-03-23 15:42:32 +0100

    fix: improve the supported languages generation

--- Tools/Toolkit/Sources/ToolkitLibrary/Models/Language.swift
+++ Tools/Toolkit/Sources/ToolkitLibrary/Models/Language.swift
@@ -16,83 +16,54 @@ enum Language {
   case dotnet
   case custom(String)
 
+  private struct Meta {
+    let name: String
+    let displayName: String
+    let icon: String?
+    let aliases: [String]
+  }
+
+  private static let knownCases: [(Language, Meta)] = [
+    (.applescript, Meta(name: "applescript", displayName: "AppleScript", icon: "icon-applescript.png", aliases: ["osascript"])),
+    (.bash,        Meta(name: "bash",        displayName: "Bash",        icon: "icon-bash.png",        aliases: ["zsh", "sh"])),
+    (.python,      Meta(name: "python",      displayName: "Python",      icon: "icon-python.png",      aliases: ["python2", "python3"])),
+    (.ruby,        Meta(name: "ruby",        displayName: "Ruby",        icon: "icon-ruby.png",        aliases: [])),
+    (.swift,       Meta(name: "swift",       displayName: "Swift",       icon: "icon-swift.png",       aliases: [])),
+    (.node,        Meta(name: "node",        displayName: "Node",        icon: "icon-nodejs.png",      aliases: ["js", "zx"])),
+    (.php,         Meta(name: "php",         displayName: "PHP",         icon: "icon-php.png",         aliases: [])),
+    (.dotnet,      Meta(name: "dotnet",      displayName: ".NET",        icon: "icon-dotnet.png",      aliases: ["cs"])),
+  ]
+
+  private var meta: Meta {
+    switch self {
+    case .custom(let value):
+      return Meta(name: value, displayName: value, icon: nil, aliases: [])
+    default:
+      return Language.knownCases.first { $0.0 == self }!.1
+    }
+  }
+
   init(_ rawValue: String) {
     let value = rawValue.lowercased()
 
-    switch value {
-    case "applescript", "osascript":
-      self = .applescript
-    case "bash", "zsh", "sh":
-      self = .bash
-    case "python", "python2", "python3":
-      self = .python
-    case "ruby":
-      self = .ruby
-    case "swift":
-      self = .swift
-    case "node", "js", "zx":
-      self = .node
-    case "php":
-      self = .php
-    case "dotnet":
-      self = .dotnet
-    default:
-      self = .custom(value)
+    for (language, meta) in Language.knownCases {
+      if meta.name == value || meta.aliases.contains(value) {
+        self = language
+        return
+      }
     }
+
+    self = .custom(value)
   }
 
-  var icon: String? {
-    switch self {
-    case .applescript:
-      return "icon-applescript.png"
-    case .bash:
-      return "icon-bash.png"
-    case .python:
-      return "icon-python.png"
-    case .ruby:
-      return "icon-ruby.png"
-    case .swift:
-      return "icon-swift.png"
-    case .node:
-      return "icon-nodejs.png"
-    case .php:
-      return "icon-php.png"
-    case .dotnet:
-      return "icon-dotnet.png"
-    default:
-      return nil
-    }
-  }
-
-  var displayName: String {
-    switch self {
-    case .applescript:
-      return "AppleScript"
-    case .bash:
-      return "Bash"
-    case .python:
-      return "Python"
-    case .ruby:
-      return "Ruby"
-    case .swift:
-      return "Swift"
-    case .node:
-      return "Node"
-    case .php:
-      return "PHP"
-    case .dotnet:
-      return ".NET"
-    case .custom(let name):
-      return name
-    }
-  }
-
-  var name: String {
-    displayName.lowercased()
-  }
+  var name: String { meta.name }
+  var displayName: String { meta.displayName }
+  var icon: String? { meta.icon }
 }
 
 // MARK: -
+
+extension Language: Equatable {}
 
 extension Language: MarkdownDescriptionProtocol {
   var markdownDescription: String {
