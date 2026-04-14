aac6fa9a4365f0b78455de6b17e1990e1cec015b
Thiago Holanda <unnamedd@gmail.com>
2026-04-14 21:19:20 +0200

    Upgrade `Toolkit` version

--- Tools/Toolkit/Sources/ToolkitLibrary/Core/Toolkit/Toolkit+Constants.swift
+++ Tools/Toolkit/Sources/ToolkitLibrary/Core/Toolkit/Toolkit+Constants.swift
@@ -29,7 +29,7 @@ public extension Toolkit {
   static var information: (name: String, version: String) {
     (
       name: "Raycast Toolkit",
-      version: "0.4.0"
+      version: "0.5.0",
     )
   }
 
